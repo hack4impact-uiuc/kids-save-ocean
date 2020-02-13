8440f7b881a8d0c2ba549cd59bbbc9e3fdae93b8
Arpan Laha <arpanlaha99@gmail.com>
2020-02-13 15:04:07 +0000

    fix reactstrap css loading

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,27 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import { Head } from "../components/Head";
-import { Input } from 'reactstrap';
+import { Button, Input } from "reactstrap";
 
-import "../public/style.scss"
+import "../public/style.scss";
 
 export default class App extends Component {
-    // const searchBar = {
-    //   flex: 1,
-    //   justifyContent: center
-    // }
-    render() {
-      return (
-        <>
-          <div>
-            <h1 align="center">
-              Welcome to FateMaker!
-            </h1>
-          </div>
+  // const searchBar = {
+  //   flex: 1,
+  //   justifyContent: center
+  // }
+  render() {
+    return (
+      <>
+        <div>
+          <h1 align="center">Welcome to FateMaker!</h1>
+        </div>
 
-          <div className="searchBar">
-            <Input type="text" className="input" placeholder="Search" />
-          </div>
-        </>
-      );
-    }
+        <div className="searchBar">
+          <Input type="text" className="input" placeholder="Search" />
+        </div>
+        <Button>Test</Button>
+      </>
+    );
+  }
 }
