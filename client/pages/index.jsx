699d34a3e8710ab9025644ad0a21d454523a0efc
sahithimuthyala <sahithi4@illinois.edu>
2020-02-12 23:15:33 -0600

    work on frontend set-up

---
+++ client/pages/index.jsx
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import { Head } from "../components/Head";
+import { Input } from 'reactstrap';
+
+import "../public/style.scss"
+
+export default class App extends Component {
+    // const searchBar = {
+    //   flex: 1,
+    //   justifyContent: center
+    // }
+    render() {
+      return (
+        <>
+          <div>
+            <h1 align="center">
+              Welcome to FateMaker!
+            </h1>
+          </div>
+
+          <div className="searchBar">
+            <Input type="text" className="input" placeholder="Search" />
+          </div>
+        </>
+      );
+    }
+}
