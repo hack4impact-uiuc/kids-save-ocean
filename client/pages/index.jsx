1e867787558e6594c8d4fb2fea52862d383ccd8d
ashwin1104 <ashwin.saxena1@gmail.com>
2020-02-22 16:49:43 -0600

    Homepage additions

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Head } from "../components/Head";
+import ReactPlayer from "react-player";
 import {
   Button,
   Card,
@@ -24,18 +25,79 @@ export default class App extends Component {
       };
       sdgImages.push(sdgObject);
     }
+    const sdgObject = {
+      id: 18,
+      imageLink: `/sdg-images/sdg.png`
+    };
+    sdgImages.push(sdgObject);
   };
 
   render() {
     this.populateImagesArr(this.sdgImages);
     return (
       <Container>
+        <Row className="header-row" xs="15" justify="center" align="middle">
+          <Col>
+            <img className="header-img" src="/homepage-images/menu-icon.png" />
+          </Col>
+          <Col>
+            <img className="header-img" src="/homepage-images/kso-icon.png" />
+          </Col>
+          <Col></Col>
+          <Col></Col>
+          <Col></Col>
+          <Col></Col>
+          <Col></Col>
+          <Col></Col>
+          <Col></Col>
+          <Col></Col>
+          <Col></Col>
+          <Col></Col>
+          <Col>
+            <img
+              className="header-img"
+              src="/homepage-images/notification-icon.png"
+            />
+          </Col>
+          <Col>
+            <img className="header-img" src="/homepage-images/user3-icon.png" />
+          </Col>
+        </Row>
         <div className="page-title">
-          <h1 align="center">
+          <h1 className="header2-text" align="center">
             <strong>FateMaker</strong>
           </h1>
         </div>
-        <Row>
+        <Row className="home-block-1" type="flex" justify="center" xs="2">
+          <Col className="home-block-col">
+            <ReactPlayer url="https://youtu.be/dQw4w9WgXcQ" />
+          </Col>
+          <Col className="home-block-col">
+            <a className="text-link" href="about us">
+              ABOUT US
+            </a>
+            <h1 className="header-text">Change the World,</h1>
+            <h1 className="header-text" mode="single">
+              one step at a time.
+            </h1>
+            <p className="body-text">
+              The barred ratio flips under the proved nail. An umbrella brushes
+              a loading ash. An anarchy purchases the compromise against the
+              simulated cheek. The village parades beneath the breakdown.
+            </p>
+            <Row type="flex">
+              <Col span={18}>
+                <Button type="primary">
+                  <strong>Find Projects</strong>
+                </Button>{" "}
+                <Button type="primary">
+                  <strong>Start a Project</strong>
+                </Button>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row className="sdg-row">
           {this.sdgImages.map(sdgImage => (
             <Col key={sdgImage.id} className="sdg-col" sm="2">
               <CardGroup>
@@ -62,6 +124,7 @@ export default class App extends Component {
             </Col>
           ))}
         </Row>
+        <img src="/homepage-images/filler-map.png" />
       </Container>
     );
   }
