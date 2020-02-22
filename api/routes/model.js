30a4889115198bbd1a6da9a58f35e32805d2905d
ashankbehara <abehara2@illinois.edu>
2020-02-22 17:28:55 -0600

    Revert "Resolved comments"
    This reverts commit 3ebc3df0380a604f931e8b485b935e427727e665.

--- api/routes/model.js
+++ api/routes/model.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const validate = require('express-jsonschema').validate;
+var validate = require('express-jsonschema').validate;
 
 
-const ModelSchema = {
+var ModelSchema = {
     id: '/ModelSchema',
     type: 'object',
     properties: {
@@ -47,7 +47,7 @@ const ModelSchema = {
       }
     }
   };
-  //GET models
+
   router.get('/', function(req, res, next) {
     const db = req.db;
     const collection = db.get('modelCollection');
@@ -55,51 +55,22 @@ const ModelSchema = {
         res.send(docs);
     });
   });
-  //GET model by ID
-  router.get('/:model_ID', function(req, res, next) {
-    const db = req.db;
-    let id = req.params.model_ID;
-    const collection = db.get('modelCollection');
-    collection.find({_id:id},{$exists: true},function(e,docs){
-        if (docs) {
-          res.send(docs);
-        } else {
-          res.sendStatus(400);
-        }
-    });
-  });
-  //GET models by SDG
-  router.get('/sdg/:sdg_num', function(req, res, next) {
-    const db = req.db;
-    let sdg_num = parseInt(req.params.sdg_num);
-    if (sdg_num == NaN) {
-      res.sendStatus(400);
-    }
-    const collection = db.get('modelCollection');
-    collection.find({sdg: { $elemMatch: { $gte: sdg_num, $lt: sdg_num + 1}}},{$exists: true},function(e,docs){
-        if (docs) {
-          res.send(docs);
-        } else {
-          res.sendStatus(400);
-        }
-    });
-  });
-  
-  //POST model
+  // TODO; Check Validate 
   router.post('/', validate({body: ModelSchema}), function(req, res, next) {
     const db = req.db;
     const collection = db.get('modelCollection');
     const data = req.body;
+
     // Check if data includes proper fields
-    collection.insert(data, function(err, obj) {
-      if (err) {
-        res.sendStatus(500);
-      }
-    });
-    res.json({ success: data.name + " added!"});
-    
+
+    if (data) {
+        collection.insert(data, function(err, obj) {});
+        res.json({ success: data.name + " added!"});
+    } else {
+        res.sendStatus(403);
+    }
   });
-  //DELTE model by ID
+
   router.delete('/:model_ID', function (req, res, next) {
     const db = req.db
     let id = req.params.model_ID;
@@ -109,11 +80,10 @@ const ModelSchema = {
             collection.remove({_id: id}, function(err, obj) {});
             res.json({success: id + ' deleted!'});
         } else {
-            res.sendStatus(400);
+            res.sendStatus(403);
         }
     })
   });
-  //UPDATE model by ID
   router.put('/:model_ID',validate({body: ModelSchema}), function(req, res, next) {
     const db = req.db;
     let id = req.params.model_ID;
@@ -123,7 +93,7 @@ const ModelSchema = {
         collection.update({_id:id},{$set: req.body});
         res.json({success: id + ' updated!'})
       } else {
-        res.sendStatus(400);
+        res.sendStatus(403);
       }
     })
   });
