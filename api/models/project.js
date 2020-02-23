8c7e63c15fcdc606923b0a1336f4190501856e9d
ashankbehara <abehara2@illinois.edu>
2020-02-22 18:17:15 -0600

    Failing validation

--- api/models/project.js
+++ api/models/project.js
@@ -1,36 +1,25 @@
-const mongoose = require("mongoose");
+const validate = require("express-jsonschema");
 
-const Project = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  SDG: { type: SDGenum, required: true },
-  stage: { type: [Stage], required: false }
-});
+// const SDGenum = {
+//     1: "NO POVERTY",
+//     2: "HUNGER",
+//     3: "GOOD HEALTH AND WELL-BEING",
+//     4: "QUALITY EDUCATION",
+//     5: "GENDER EQUALITY",
+//     6: "CLEAN WATER AND SANITATION",
+//     7: "AFFORDABLE AND CLEAN CLEAN ENERGY",
+//     8: "DECENT WORK AND ECONOMIC GROWTH",
+//     9: "INDUSTRY, INNOVATION, AND INFRASTRUCTURE",
+//     10: "REDUCED INEQUALITIES",
+//     11: "SUSTAINABLE CITIES AND COMMUNITIES",
+//     12: "RESPONSIBLE CONSUMPTION AND PRODUCTION",
+//     13: "CLIMATE ACTION",
+//     14: "LIFE BELOW WATER",
+//     15: "LIFE ON LAND",
+//     16: "PEACE, JUSTICE, AND STRONG INSTITUTIONS",
+//     17: "PARTNERSHIPS FOR THE GOALS"
+//   };
 
-const Stage = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  stakeholders: { type: [String], required: false },
-  challenges: { type: [String], required: false },
-  insights: { type: [String], required: false }
-});
 
-const SDGenum = {
-  1: "NO POVERTY",
-  2: "HUNGER",
-  3: "GOOD HEALTH AND WELL-BEING",
-  4: "QUALITY EDUCATION",
-  5: "GENDER EQUALITY",
-  6: "CLEAN WATER AND SANITATION",
-  7: "AFFORDABLE AND CLEAN CLEAN ENERGY",
-  8: "DECENT WORK AND ECONOMIC GROWTH",
-  9: "INDUSTRY, INNOVATION, AND INFRASTRUCTURE",
-  10: "REDUCED INEQUALITIES",
-  11: "SUSTAINABLE CITIES AND COMMUNITIES",
-  12: "RESPONSIBLE CONSUMPTION AND PRODUCTION",
-  13: "CLIMATE ACTION",
-  14: "LIFE BELOW WATER",
-  15: "LIFE ON LAND",
-  16: "PEACE, JUSTICE, AND STRONG INSTITUTIONS",
-  17: "PARTNERSHIPS FOR THE GOALS"
-};
+
+module.exports = { Project: Project };
