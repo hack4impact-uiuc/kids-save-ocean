96afbd1fd2d75b6cf09ed23aeed40767e9578e38
Yousef Ahmed <yousefahmed0066@gmail.com>
2020-02-22 18:46:11 -0600

    Fix validator issue

--- api/app.js
+++ api/app.js
@@ -1,37 +1,36 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-const modelRouter = require('./routes/model');
+const indexRouter = require("./routes/index");
+const modelRouter = require("./routes/model");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-const monk = require('monk');
-const db = monk('localhost:27017/kids-save-ocean');
+const monk = require("monk");
+const db = monk("localhost:27017/kids-save-ocean");
 
-
-app.use(function(req,res,next){
+app.use(function(req, res, next) {
   req.db = db;
   next();
 });
 
-app.use('/', indexRouter);
-app.use('/models', modelRouter);
+app.use("/", indexRouter);
+app.use("/models", modelRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,11 +41,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- api/routes/model.js
+++ api/routes/model.js
@@ -1,101 +1,178 @@
 const express = require('express');
 const router = express.Router();
-var validate = require('express-jsonschema').validate;
+const validate = require('express-jsonschema').validate;
 
 
-var ModelSchema = {
-    id: '/ModelSchema',
-    type: 'object',
-    properties: {
-      name: {
-        type: 'string',
-        required: true
+const ModelSchema = {
+  id: '/ModelSchema',
+  type: 'object',
+  properties: {
+    name: {
+      type: 'string',
+      required: true
+    },
+    sdg: {
+      type: 'array',
+      items: {
+        type: 'integer'
       },
-      sdg: {
-        type: 'array',
-        items:{type: 'integer'},
-        required: true
-      },
-      description: {
-        type: 'string',
-        required: true
-      },
-      stages: {
-        type: 'object',
-        patternProperties: {
-          ".*": {
-            type: 'object',
-            properties: {
-              stakeholders: {
-                type: 'array',
-                items: {type: 'string'}
-              },
-              challenges: {
-                type: 'array',
-                items: {type: 'string'}
-              },
-              insights: {
-                type: 'array',
-                items: {type: 'string'}
-              },
-              description: {
+      required: true
+    },
+    description: {
+      type: 'string',
+      required: true
+    },
+    stages: {
+      type: 'object',
+      patternProperties: {
+        ".*": {
+          type: 'object',
+          properties: {
+            stakeholders: {
+              type: 'array',
+              items: {
                 type: 'string'
               }
+            },
+            challenges: {
+              type: 'array',
+              items: {
+                type: 'string'
+              }
+            },
+            insights: {
+              type: 'array',
+              items: {
+                type: 'string'
+              }
+            },
+            description: {
+              type: 'string'
             }
           }
         }
       }
     }
-  };
+  }
+};
 
-  router.get('/', function(req, res, next) {
-    const db = req.db;
-    const collection = db.get('modelCollection');
-    collection.find({},{},function(e,docs){
-        res.send(docs);
-    });
+router.get('/', function (req, res, next) {
+  const db = req.db;
+  const collection = db.get('modelCollection');
+  collection.find({}, {}, function (e, docs) {
+    res.send(docs);
   });
-  // TODO; Check Validate 
-  router.post('/', validate({body: ModelSchema}), function(req, res, next) {
-    const db = req.db;
-    const collection = db.get('modelCollection');
-    const data = req.body;
+});
 
-    // Check if data includes proper fields
-
-    if (data) {
-        collection.insert(data, function(err, obj) {});
-        res.json({ success: data.name + " added!"});
+router.get("/:model_ID", function (req, res, next) {
+  const db = req.db;
+  let id = req.params.model_ID;
+  const collection = db.get("modelCollection");
+  collection.find({
+    _id: id
+  }, {
+    $exists: true
+  }, function (e, docs) {
+    if (docs) {
+      res.send(docs);
     } else {
-        res.sendStatus(403);
+      res.sendStatus(400);
     }
   });
-
-  router.delete('/:model_ID', function (req, res, next) {
-    const db = req.db
-    let id = req.params.model_ID;
-    const collection = db.get('modelCollection');
-    collection.find({_id: id}, {$exists: true}, function(e,docs) {
-        if (docs) {
-            collection.remove({_id: id}, function(err, obj) {});
-            res.json({success: id + ' deleted!'});
-        } else {
-            res.sendStatus(403);
+});
+//GET models by SDG
+router.get("/sdg/:sdg_num", function (req, res, next) {
+  const db = req.db;
+  let sdg_num = parseInt(req.params.sdg_num);
+  if (sdg_num == NaN) {
+    res.sendStatus(400);
+  }
+  const collection = db.get("modelCollection");
+  collection.find({
+      sdg: {
+        $elemMatch: {
+          $gte: sdg_num,
+          $lt: sdg_num + 1
         }
-    })
-  });
-  router.put('/:model_ID',validate({body: ModelSchema}), function(req, res, next) {
-    const db = req.db;
-    let id = req.params.model_ID;
-    const collection = db.get('modelCollection');
-    collection.find({_id: id}, {$exists: true}, function(e,docs) {
-      if (docs) {
-        collection.update({_id:id},{$set: req.body});
-        res.json({success: id + ' updated!'})
-      } else {
-        res.sendStatus(403);
       }
-    })
-  });
-  
-  module.exports = router;
+    }, {
+      $exists: true
+    },
+    function (e, docs) {
+      if (docs) {
+        res.send(docs);
+      } else {
+        res.sendStatus(400);
+      }
+    }
+  );
+});
+
+
+// TODO; Check Validate 
+router.post('/', validate({
+  body: ModelSchema
+}), function (req, res, next) {
+  const db = req.db;
+  const collection = db.get('modelCollection');
+  const data = req.body;
+
+  // Check if data includes proper fields
+  collection.insert(data, function (err, obj) {});
+  if (err) {
+    res.sendStatus(500);
+  } else {
+    res.json({
+      success: data.name + " added!"
+    });
+  }
+});
+
+router.delete('/:model_ID', function (req, res, next) {
+  const db = req.db
+  let id = req.params.model_ID;
+  const collection = db.get('modelCollection');
+  collection.find({
+    _id: id
+  }, {
+    $exists: true
+  }, function (e, docs) {
+    if (docs) {
+      collection.remove({
+        _id: id
+      }, function (err, obj) {});
+      res.json({
+        success: id + ' deleted!'
+      });
+    } else {
+      res.sendStatus(400);
+    }
+  })
+});
+router.put('/:model_ID', validate({
+  body: ModelSchema
+}), function (req, res, next) {
+  const db = req.db;
+  let id = req.params.model_ID;
+  const collection = db.get('modelCollection');
+  collection.find({
+    _id: id
+  }, {
+    $exists: true
+  }, function (e, docs) {
+    if (docs) {
+      collection.update({
+        _id: id
+      }, {
+        $set: req.body
+      });
+      res.json({
+        success: id + ' updated!'
+      })
+    } else {
+      res.sendStatus(400);
+    }
+  })
+});
+
+module.exports = router;
