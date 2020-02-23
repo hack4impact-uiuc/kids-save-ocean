f76e2c33d7768a3a8ece5261bc4bfe252bba9da1
ashankbehara <abehara2@illinois.edu>
2020-02-22 18:53:51 -0600

    Fixed wrapper

--- client/utils/apiWrapper.js
+++ client/utils/apiWrapper.js
@@ -7,7 +7,7 @@ export const getModels = () => {
    * Returns all models
    * Returns GET_MODEL_FAIL upon failure
    */
-  const requestString = `${BASE_URL}models`;
+  const requestString = `${BASE_URL}/models`;
   return axios
     .get(requestString, {
       headers: {
@@ -18,6 +18,70 @@ export const getModels = () => {
       return {
         type: "GET_MODEL_FAIL",
         error
+      };
+    });
+};
+export const addModel = (data) => {
+  /**
+   * Adds a form
+   * Returns POST_FORM_DATA_FAIL upon failure
+   */
+  const requestString = `${BASE_URL}/models`;
+  return axios
+    .post(
+      requestString,
+      data,
+      {
+        headers: {
+          'Content-Type': 'application/JSON',
+        },
+      },
+    )
+    .catch(error => {
+      return {
+        type: 'POST_MODEL_FAIL',
+        error,
+      };
+    });
+};
+export const editModel = (data) => {
+  /**
+   * Edits a form
+   * Returns PUT_FORM_DATA_FAIL upon failure
+   */
+  const requestString = `${BASE_URL}/models/${Model_ID}`;
+  return axios
+    .put(
+      requestString,
+      data,
+      {
+        headers: {
+          'Content-Type': 'application/JSON',
+        },
+      },
+    )
+    .catch(error => {
+      return {
+        type: 'UPDATE_MODEL_FAIL',
+        error,
+      };
+    });
+};
+export const deleteForm = Model_ID => {
+  /**
+   * Deletes a form
+   * Returns POST_FORM_DATA_FAIL upon failure
+   */
+  const requestString = `${BASE_URL}/models/${Model_ID}`;
+  return axios
+    .delete(requestString, {
+      headers: {
+      },
+    })
+    .catch(error => {
+      return {
+        type: 'DELETE_MODEL_FAIL',
+        error,
       };
     });
 };
