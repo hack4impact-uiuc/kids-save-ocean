8db80617a77eaa5d2c976de3d952960b2ae6d626
Arpan Laha <arpanlaha99@gmail.com>
2020-02-25 02:08:03 +0000

    add 17 to list

--- client/utils/mockData.js
+++ client/utils/mockData.js
@@ -4,7 +4,7 @@
 
 /* eslint-disable no-magic-numbers */
 
-export default [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(
+export default [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17].map(
   sdg => ({
     name: `Test ${sdg}`,
     sdg,
