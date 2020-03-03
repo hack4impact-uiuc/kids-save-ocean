c1cde6102476ecb30d185c95b9db9b84cdfacc9a
Arpan Laha <arpanlaha99@gmail.com>
2020-03-03 02:30:32 +0000

    add updated mock data

--- client/utils/mockData.js
+++ client/utils/mockData.js
@@ -10,7 +10,135 @@ export default [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17].map(
     sdg,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed rhoncus metus. Pellentesque vel tempor lorem. Nam mi diam, dictum in tellus ac, tempus congue turpis. Morbi tortor nunc, imperdiet eget leo ac, rhoncus vehicula risus. Vestibulum a imperdiet augue. Nam urna sem, dapibus eget sem et, sodales imperdiet nulla. Vivamus non venenatis ante, vel luctus nunc. Praesent quis sem in ante tristique gravida. Ut hendrerit pellentesque est, in ullamcorper metus rutrum eu. Curabitur et quam libero. Pellentesque non massa eleifend, ultrices risus eget, lobortis erat. Nulla facilisi. Suspendisse malesuada diam lectus, eget pulvinar libero efficitur vitae. Curabitur luctus ac dolor in molestie. Donec et tempus eros.",
-    stages: {
+    stages: [
+      {
+        name: "Plan",
+        section: "Inspiration",
+        duration: Math.random(),
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Suspendisse id vulputate libero, eu facilisis mi.Duis vehicula augue eu suscipit malesuada.Aenean at mi eget turpis tristique consectetur.Proin arcu elit, ultrices at mi in, pulvinar eleifend velit.Vestibulum sagittis quis dui ac aliquam.Sed sed libero vitae lectus porta viverra.Nulla vestibulum nibh diam, eget tincidunt arcu malesuada nec.Aenean ac eleifend purus, tincidunt elementum ligula.Maecenas velit nulla, volutpat nec semper ac, consequat nec nibh.Curabitur imperdiet bibendum nibh, at luctus erat dapibus id.Pellentesque tincidunt id urna pulvinar malesuada.Sed non ante at ipsum vulputate eleifend."
+      },
+      {
+        name: "Interviews",
+        section: "Inspiration",
+        duration: Math.random(),
+        description:
+          "Quisque tempus eu arcu sed consectetur. Curabitur non fermentum risus. Donec et pellentesque elit. Sed porta pellentesque nibh. Phasellus ultrices neque nec massa pharetra ultrices. Suspendisse laoreet arcu non tempus malesuada. Morbi ultrices nunc sapien, sit amet rutrum ante auctor tristique. Fusce consectetur efficitur sem, quis laoreet elit imperdiet viverra. Praesent imperdiet nisl vel lacus tincidunt commodo. Aenean lacinia at lacus luctus lacinia."
+      },
+      {
+        name: "Frameworks",
+        section: "Inspiration",
+        duration: Math.random(),
+        description:
+          "Pellentesque at sapien dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed fringilla dapibus varius. Quisque a ultrices ex. Nulla facilisi. Cras purus arcu, fermentum id ultrices et, gravida sit amet ligula. Suspendisse sit amet imperdiet mauris, eu semper tellus. In et est et ante mattis finibus. Donec ullamcorper urna nulla, ut tincidunt nibh convallis tincidunt. Phasellus malesuada libero sagittis, volutpat sapien sollicitudin, dictum neque. Pellentesque laoreet, tortor in mattis laoreet, tellus sem fermentum mauris, at tempor nisl odio pellentesque felis. Proin hendrerit urna nec nulla commodo accumsan. Mauris dapibus ultricies ligula, vitae iaculis sapien eleifend eget. Integer vel mauris volutpat, commodo augue non, molestie velit. Vestibulum imperdiet pulvinar felis eu commodo."
+      },
+      {
+        name: "Mash-ups",
+        section: "Inspiration",
+        duration: Math.random(),
+        description:
+          "Pellentesque at sapien dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed fringilla dapibus varius. Quisque a ultrices ex. Nulla facilisi. Cras purus arcu, fermentum id ultrices et, gravida sit amet ligula. Suspendisse sit amet imperdiet mauris, eu semper tellus. In et est et ante mattis finibus. Donec ullamcorper urna nulla, ut tincidunt nibh convallis tincidunt. Phasellus malesuada libero sagittis, volutpat sapien sollicitudin, dictum neque. Pellentesque laoreet, tortor in mattis laoreet, tellus sem fermentum mauris, at tempor nisl odio pellentesque felis. Proin hendrerit urna nec nulla commodo accumsan. Mauris dapibus ultricies ligula, vitae iaculis sapien eleifend eget. Integer vel mauris volutpat, commodo augue non, molestie velit. Vestibulum imperdiet pulvinar felis eu commodo."
+      },
+      {
+        name: "Pilot",
+        section: "Inspiration",
+        duration: Math.random(),
+        description:
+          "Maecenas sit amet turpis a lacus vehicula fermentum. In dignissim elementum nisl, in laoreet nisi ultricies ac. Integer id dapibus nisi. Nullam lacinia ex ac nunc dapibus, vel molestie orci egestas. In facilisis lobortis libero vitae eleifend. Nulla non lectus fermentum, semper metus a, vehicula metus. Aenean purus dui, pulvinar eget augue et, ultrices auctor urna. Etiam posuere, elit quis tincidunt molestie, neque neque auctor orci, in mollis turpis sem eget turpis. Nam euismod purus a volutpat laoreet. Fusce diam erat, tempor sit amet laoreet ut, sagittis eu sem."
+      },
+      {
+        name: "Pitch",
+        section: "Inspiration",
+        duration: Math.random(),
+        description:
+          "Praesent efficitur porta sem non commodo. Suspendisse non velit euismod, porttitor urna sit amet, malesuada eros. Duis non tincidunt nisi. Cras facilisis odio et neque ultricies luctus. Vestibulum dignissim ante a felis ornare, sit amet luctus nunc sagittis. Fusce convallis mauris massa, a consectetur tortor gravida auctor. Integer congue ipsum at augue ultrices fringilla. Aenean consequat massa ac justo hendrerit, sit amet consectetur tellus aliquet. Vestibulum pulvinar elementum tempus. Etiam id feugiat enim, vitae facilisis ante. Vestibulum egestas faucibus feugiat. Nunc leo diam, hendrerit nec eros vitae, eleifend dictum lectus. Praesent a elementum dolor, vitae varius ipsum. In tempor egestas erat quis tincidunt."
+      },
+      {
+        name: "Plan",
+        section: "Inspiration",
+        duration: Math.random(),
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Suspendisse id vulputate libero, eu facilisis mi.Duis vehicula augue eu suscipit malesuada.Aenean at mi eget turpis tristique consectetur.Proin arcu elit, ultrices at mi in, pulvinar eleifend velit.Vestibulum sagittis quis dui ac aliquam.Sed sed libero vitae lectus porta viverra.Nulla vestibulum nibh diam, eget tincidunt arcu malesuada nec.Aenean ac eleifend purus, tincidunt elementum ligula.Maecenas velit nulla, volutpat nec semper ac, consequat nec nibh.Curabitur imperdiet bibendum nibh, at luctus erat dapibus id.Pellentesque tincidunt id urna pulvinar malesuada.Sed non ante at ipsum vulputate eleifend."
+      },
+      {
+        name: "Interviews",
+        section: "Ideation",
+        duration: Math.random(),
+        description:
+          "Quisque tempus eu arcu sed consectetur. Curabitur non fermentum risus. Donec et pellentesque elit. Sed porta pellentesque nibh. Phasellus ultrices neque nec massa pharetra ultrices. Suspendisse laoreet arcu non tempus malesuada. Morbi ultrices nunc sapien, sit amet rutrum ante auctor tristique. Fusce consectetur efficitur sem, quis laoreet elit imperdiet viverra. Praesent imperdiet nisl vel lacus tincidunt commodo. Aenean lacinia at lacus luctus lacinia."
+      },
+      {
+        name: "Frameworks",
+        section: "Ideation",
+        duration: Math.random(),
+        description:
+          "Pellentesque at sapien dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed fringilla dapibus varius. Quisque a ultrices ex. Nulla facilisi. Cras purus arcu, fermentum id ultrices et, gravida sit amet ligula. Suspendisse sit amet imperdiet mauris, eu semper tellus. In et est et ante mattis finibus. Donec ullamcorper urna nulla, ut tincidunt nibh convallis tincidunt. Phasellus malesuada libero sagittis, volutpat sapien sollicitudin, dictum neque. Pellentesque laoreet, tortor in mattis laoreet, tellus sem fermentum mauris, at tempor nisl odio pellentesque felis. Proin hendrerit urna nec nulla commodo accumsan. Mauris dapibus ultricies ligula, vitae iaculis sapien eleifend eget. Integer vel mauris volutpat, commodo augue non, molestie velit. Vestibulum imperdiet pulvinar felis eu commodo."
+      },
+      {
+        name: "Mash-ups",
+        section: "Ideation",
+        duration: Math.random(),
+        description:
+          "Pellentesque at sapien dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed fringilla dapibus varius. Quisque a ultrices ex. Nulla facilisi. Cras purus arcu, fermentum id ultrices et, gravida sit amet ligula. Suspendisse sit amet imperdiet mauris, eu semper tellus. In et est et ante mattis finibus. Donec ullamcorper urna nulla, ut tincidunt nibh convallis tincidunt. Phasellus malesuada libero sagittis, volutpat sapien sollicitudin, dictum neque. Pellentesque laoreet, tortor in mattis laoreet, tellus sem fermentum mauris, at tempor nisl odio pellentesque felis. Proin hendrerit urna nec nulla commodo accumsan. Mauris dapibus ultricies ligula, vitae iaculis sapien eleifend eget. Integer vel mauris volutpat, commodo augue non, molestie velit. Vestibulum imperdiet pulvinar felis eu commodo."
+      },
+      {
+        name: "Pilot",
+        section: "Ideation",
+        duration: Math.random(),
+        description:
+          "Maecenas sit amet turpis a lacus vehicula fermentum. In dignissim elementum nisl, in laoreet nisi ultricies ac. Integer id dapibus nisi. Nullam lacinia ex ac nunc dapibus, vel molestie orci egestas. In facilisis lobortis libero vitae eleifend. Nulla non lectus fermentum, semper metus a, vehicula metus. Aenean purus dui, pulvinar eget augue et, ultrices auctor urna. Etiam posuere, elit quis tincidunt molestie, neque neque auctor orci, in mollis turpis sem eget turpis. Nam euismod purus a volutpat laoreet. Fusce diam erat, tempor sit amet laoreet ut, sagittis eu sem."
+      },
+      {
+        name: "Pitch",
+        section: "Ideation",
+        duration: Math.random(),
+        description:
+          "Praesent efficitur porta sem non commodo. Suspendisse non velit euismod, porttitor urna sit amet, malesuada eros. Duis non tincidunt nisi. Cras facilisis odio et neque ultricies luctus. Vestibulum dignissim ante a felis ornare, sit amet luctus nunc sagittis. Fusce convallis mauris massa, a consectetur tortor gravida auctor. Integer congue ipsum at augue ultrices fringilla. Aenean consequat massa ac justo hendrerit, sit amet consectetur tellus aliquet. Vestibulum pulvinar elementum tempus. Etiam id feugiat enim, vitae facilisis ante. Vestibulum egestas faucibus feugiat. Nunc leo diam, hendrerit nec eros vitae, eleifend dictum lectus. Praesent a elementum dolor, vitae varius ipsum. In tempor egestas erat quis tincidunt."
+      },
+      {
+        name: "Plan",
+        section: "Implementation",
+        duration: Math.random(),
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Suspendisse id vulputate libero, eu facilisis mi.Duis vehicula augue eu suscipit malesuada.Aenean at mi eget turpis tristique consectetur.Proin arcu elit, ultrices at mi in, pulvinar eleifend velit.Vestibulum sagittis quis dui ac aliquam.Sed sed libero vitae lectus porta viverra.Nulla vestibulum nibh diam, eget tincidunt arcu malesuada nec.Aenean ac eleifend purus, tincidunt elementum ligula.Maecenas velit nulla, volutpat nec semper ac, consequat nec nibh.Curabitur imperdiet bibendum nibh, at luctus erat dapibus id.Pellentesque tincidunt id urna pulvinar malesuada.Sed non ante at ipsum vulputate eleifend."
+      },
+      {
+        name: "Interviews",
+        section: "Implementation",
+        duration: Math.random(),
+        description:
+          "Quisque tempus eu arcu sed consectetur. Curabitur non fermentum risus. Donec et pellentesque elit. Sed porta pellentesque nibh. Phasellus ultrices neque nec massa pharetra ultrices. Suspendisse laoreet arcu non tempus malesuada. Morbi ultrices nunc sapien, sit amet rutrum ante auctor tristique. Fusce consectetur efficitur sem, quis laoreet elit imperdiet viverra. Praesent imperdiet nisl vel lacus tincidunt commodo. Aenean lacinia at lacus luctus lacinia."
+      },
+      {
+        name: "Frameworks",
+        section: "Implementation",
+        duration: Math.random(),
+        description:
+          "Pellentesque at sapien dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed fringilla dapibus varius. Quisque a ultrices ex. Nulla facilisi. Cras purus arcu, fermentum id ultrices et, gravida sit amet ligula. Suspendisse sit amet imperdiet mauris, eu semper tellus. In et est et ante mattis finibus. Donec ullamcorper urna nulla, ut tincidunt nibh convallis tincidunt. Phasellus malesuada libero sagittis, volutpat sapien sollicitudin, dictum neque. Pellentesque laoreet, tortor in mattis laoreet, tellus sem fermentum mauris, at tempor nisl odio pellentesque felis. Proin hendrerit urna nec nulla commodo accumsan. Mauris dapibus ultricies ligula, vitae iaculis sapien eleifend eget. Integer vel mauris volutpat, commodo augue non, molestie velit. Vestibulum imperdiet pulvinar felis eu commodo."
+      },
+      {
+        name: "Mash-ups",
+        section: "Implementation",
+        duration: Math.random(),
+        description:
+          "Pellentesque at sapien dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed fringilla dapibus varius. Quisque a ultrices ex. Nulla facilisi. Cras purus arcu, fermentum id ultrices et, gravida sit amet ligula. Suspendisse sit amet imperdiet mauris, eu semper tellus. In et est et ante mattis finibus. Donec ullamcorper urna nulla, ut tincidunt nibh convallis tincidunt. Phasellus malesuada libero sagittis, volutpat sapien sollicitudin, dictum neque. Pellentesque laoreet, tortor in mattis laoreet, tellus sem fermentum mauris, at tempor nisl odio pellentesque felis. Proin hendrerit urna nec nulla commodo accumsan. Mauris dapibus ultricies ligula, vitae iaculis sapien eleifend eget. Integer vel mauris volutpat, commodo augue non, molestie velit. Vestibulum imperdiet pulvinar felis eu commodo."
+      },
+      {
+        name: "Pilot",
+        section: "Implementation",
+        duration: Math.random(),
+        description:
+          "Maecenas sit amet turpis a lacus vehicula fermentum. In dignissim elementum nisl, in laoreet nisi ultricies ac. Integer id dapibus nisi. Nullam lacinia ex ac nunc dapibus, vel molestie orci egestas. In facilisis lobortis libero vitae eleifend. Nulla non lectus fermentum, semper metus a, vehicula metus. Aenean purus dui, pulvinar eget augue et, ultrices auctor urna. Etiam posuere, elit quis tincidunt molestie, neque neque auctor orci, in mollis turpis sem eget turpis. Nam euismod purus a volutpat laoreet. Fusce diam erat, tempor sit amet laoreet ut, sagittis eu sem."
+      },
+      {
+        name: "Pitch",
+        section: "Implementation",
+        duration: Math.random(),
+        description:
+          "Praesent efficitur porta sem non commodo. Suspendisse non velit euismod, porttitor urna sit amet, malesuada eros. Duis non tincidunt nisi. Cras facilisis odio et neque ultricies luctus. Vestibulum dignissim ante a felis ornare, sit amet luctus nunc sagittis. Fusce convallis mauris massa, a consectetur tortor gravida auctor. Integer congue ipsum at augue ultrices fringilla. Aenean consequat massa ac justo hendrerit, sit amet consectetur tellus aliquet. Vestibulum pulvinar elementum tempus. Etiam id feugiat enim, vitae facilisis ante. Vestibulum egestas faucibus feugiat. Nunc leo diam, hendrerit nec eros vitae, eleifend dictum lectus. Praesent a elementum dolor, vitae varius ipsum. In tempor egestas erat quis tincidunt."
+      }
+    ],
+    sections: {
       inspiration: {
         description:
           "Cras convallis sollicitudin neque, nec iaculis sem porta at. Etiam sit amet ante ipsum. Curabitur fermentum justo quis nibh imperdiet, vitae pellentesque felis fermentum. Duis iaculis auctor nunc, sit amet finibus odio sodales eget. Phasellus porttitor auctor nisl non finibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin at consequat nunc. Pellentesque tempus quam id mattis feugiat. Integer elementum justo ut velit feugiat porttitor.",
