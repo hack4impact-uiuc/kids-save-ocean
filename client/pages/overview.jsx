cf9a8d2558a853dac4d295d4a72bbe693ef4b305
Arpan Laha <arpanlaha99@gmail.com>
2020-03-05 03:34:33 +0000

    fix width and center

--- client/pages/overview.jsx
+++ client/pages/overview.jsx
@@ -1,12 +1,35 @@
+import { useEffect, useState } from "react";
 import { Chart } from "react-google-charts";
 import mockData from "../utils/mockData";
 
 import "../public/styles/overview.scss";
 
-const ganttData = mockData[0].stages.map(stage => [
-  `${stage.name} (${stage.section})`,
+const inspirationData = mockData[0].inspiration.stages.map(stage => [
+  `${stage.name} (Inspiration)`,
   stage.name,
-  stage.section,
+  "Inspiration",
+  new Date(stage.startTime * 1000),
+  new Date((stage.startTime + stage.duration) * 1000),
+  null,
+  Math.random() * 100,
+  null
+]);
+
+const ideationData = mockData[0].ideation.stages.map(stage => [
+  `${stage.name} (Ideation)`,
+  stage.name,
+  "Ideation",
+  new Date(stage.startTime * 1000),
+  new Date((stage.startTime + stage.duration) * 1000),
+  null,
+  Math.random() * 100,
+  null
+]);
+
+const implementationData = mockData[0].implementation.stages.map(stage => [
+  `${stage.name} (Implementation)`,
+  stage.name,
+  "Implementation",
   new Date(stage.startTime * 1000),
   new Date((stage.startTime + stage.duration) * 1000),
   null,
@@ -15,12 +38,23 @@ const ganttData = mockData[0].stages.map(stage => [
 ]);
 
 export default function Overview(props) {
+  // console.log(`${document.body.clientWidth * 2.4}px`);
+  const [width, setWidth] = useState(null);
+  if (process.browser) {
+    useEffect(() => setWidth(document.children[0].clientWidth), [
+      document.children[0].clientWidth
+    ]);
+  }
   return (
     <div className="gantt-container">
       <Chart
         className="gantt-chart"
-        height={`${ganttData.length * 30 + 50}px`}
-        width="1000px"
+        height={`${(inspirationData.length +
+          ideationData.length +
+          implementationData.length) *
+          30 +
+          50}px`}
+        width={`${width * 2.4}px`}
         chartType="Gantt"
         loader={<div>Loading Chart</div>}
         data={[
@@ -34,7 +68,9 @@ export default function Overview(props) {
             { type: "number", label: "Percent Complete" },
             { type: "string", label: "Dependencies" }
           ],
-          ...ganttData
+          ...inspirationData,
+          ...ideationData,
+          ...implementationData
         ]}
         options={{
           gantt: {
