586b28bb533f89b1a6dff6b10771ff80bf16a9a4
Arpan Laha <arpanlaha99@gmail.com>
2020-03-05 04:53:23 +0000

    open modal on click

--- client/pages/overview.jsx
+++ client/pages/overview.jsx
@@ -1,84 +1,149 @@
 import { useEffect, useState } from "react";
+import { Head } from "../components";
 import { Chart } from "react-google-charts";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
+import classnames from "classnames";
+
 import mockData from "../utils/mockData";
 
 import "../public/styles/overview.scss";
 
-const inspirationData = mockData[0].inspiration.stages.map(stage => [
-  `${stage.name} (Inspiration)`,
-  stage.name,
-  "Inspiration",
-  new Date(stage.startTime * 1000),
-  new Date((stage.startTime + stage.duration) * 1000),
-  null,
-  Math.random() * 100,
-  null
-]);
+const mapGanttData = phase =>
+  mockData[0][phase.toLowerCase()].stages.map(stage => [
+    `${stage.name} (${phase})`,
+    stage.name,
+    phase,
+    new Date(stage.startTime * 1000),
+    new Date((stage.startTime + stage.duration) * 1000),
+    null,
+    Math.random() * 100,
+    null
+  ]);
 
-const ideationData = mockData[0].ideation.stages.map(stage => [
-  `${stage.name} (Ideation)`,
-  stage.name,
-  "Ideation",
-  new Date(stage.startTime * 1000),
-  new Date((stage.startTime + stage.duration) * 1000),
-  null,
-  Math.random() * 100,
-  null
-]);
+const ganttData = {
+  Inspiration: mapGanttData("Inspiration"),
+  Ideation: mapGanttData("Ideation"),
+  Implementation: mapGanttData("Implementation")
+};
 
-const implementationData = mockData[0].implementation.stages.map(stage => [
-  `${stage.name} (Implementation)`,
-  stage.name,
-  "Implementation",
-  new Date(stage.startTime * 1000),
-  new Date((stage.startTime + stage.duration) * 1000),
-  null,
-  Math.random() * 100,
-  null
-]);
+const TRACK_HEIGHT = 50;
+const DESCRIPTION_LENGTH = 400;
 
 export default function Overview(props) {
-  // console.log(`${document.body.clientWidth * 2.4}px`);
   const [width, setWidth] = useState(null);
+  const [activePhase, setActivePhase] = useState("Inspiration");
+  const [activeStage, setActiveStage] = useState(null);
+  const [modal, setModal] = useState(false);
+
+  const toggleActivePhase = phase => {
+    if (activePhase !== phase) {
+      setActivePhase(phase);
+    }
+  };
+
+  const toggleActiveStage = stage => setActiveStage(stage);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
   if (process.browser) {
-    useEffect(() => setWidth(document.children[0].clientWidth), [
-      document.children[0].clientWidth
+    useEffect(() => setWidth(document.body.clientWidth), [
+      document.body.clientWidth
     ]);
   }
   return (
-    <div className="gantt-container">
-      <Chart
-        className="gantt-chart"
-        height={`${(inspirationData.length +
-          ideationData.length +
-          implementationData.length) *
-          30 +
-          50}px`}
-        width={`${width * 2.4}px`}
-        chartType="Gantt"
-        loader={<div>Loading Chart</div>}
-        data={[
-          [
-            { type: "string", label: "Task ID" },
-            { type: "string", label: "Task Name" },
-            { type: "string", label: "Resource" },
-            { type: "date", label: "Start Date" },
-            { type: "date", label: "End Date" },
-            { type: "number", label: "Duration" },
-            { type: "number", label: "Percent Complete" },
-            { type: "string", label: "Dependencies" }
-          ],
-          ...inspirationData,
-          ...ideationData,
-          ...implementationData
-        ]}
-        options={{
-          gantt: {
-            trackHeight: 30
-          }
-        }}
-        rootProps={{ "data-testid": "2" }}
-      />
-    </div>
+    <>
+      <Head title="Overview" />
+
+      <Modal isOpen={modal} toggle={toggleModal}>
+        <ModalHeader>{activeStage?.name}</ModalHeader>
+        <ModalBody>{`${activeStage?.description.slice(0, DESCRIPTION_LENGTH)}${
+          activeStage?.description.length > DESCRIPTION_LENGTH ? "..." : ""
+        }`}</ModalBody>
+        <ModalFooter>
+          <a>
+            <Button color="primary">See more</Button>
+          </a>
+          <Button onClick={toggleModal} color="danger">
+            Exit
+          </Button>
+        </ModalFooter>
+      </Modal>
+
+      <div className="gantt-container">
+        <Nav tabs>
+          {["Inspiration", "Ideation", "Implementation"].map(phase => (
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activePhase === phase }, "tab")}
+                onClick={() => {
+                  toggleActivePhase(phase);
+                }}
+              >
+                {phase}
+              </NavLink>
+            </NavItem>
+          ))}
+        </Nav>
+
+        <Chart
+          className="gantt-chart"
+          height={`${ganttData[activePhase].length * TRACK_HEIGHT + 50}px`}
+          width={`${width * 0.8}px`}
+          chartType="Gantt"
+          loader={<div>Loading Chart</div>}
+          data={[
+            [
+              { type: "string", label: "Task ID" },
+              { type: "string", label: "Task Name" },
+              { type: "string", label: "Resource" },
+              { type: "date", label: "Start Date" },
+              { type: "date", label: "End Date" },
+              { type: "number", label: "Duration" },
+              { type: "number", label: "Percent Complete" },
+              { type: "string", label: "Dependencies" }
+            ],
+            ...ganttData[activePhase]
+          ]}
+          chartEvents={[
+            {
+              eventName: "select",
+              callback: ({ chartWrapper }) => {
+                const selection = chartWrapper.getChart().getSelection();
+                if ((selection.length = 1)) {
+                  toggleActiveStage(
+                    mockData[0][activePhase.toLowerCase()].stages[
+                      selection[0].row
+                    ]
+                  );
+                  console.log(activeStage);
+                  console.log(
+                    mockData[0][activePhase.toLowerCase()].stages[
+                      selection[0].row
+                    ]
+                  );
+                  toggleModal();
+                }
+              }
+            }
+          ]}
+          options={{
+            gantt: {
+              trackHeight: TRACK_HEIGHT
+            }
+          }}
+        />
+      </div>
+    </>
   );
 }
