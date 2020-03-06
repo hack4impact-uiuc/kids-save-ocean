1d8f34bccec8eb5ff966e2cb11d3eac827fae82b
Arpan Laha <arpanlaha99@gmail.com>
2020-03-06 07:27:39 +0000

    remove outdated pages

--- client/pages/inspiration.jsx
+++
@@ -1,6 +0,0 @@
-import { Stage } from "../components";
-import mockData from "../utils/mockData";
-
-export default function Inspiration(props) {
-  return <Stage data={mockData[0].inspiration}></Stage>;
-}
