9cace19a4eee27bf8a60050dc8f0d8b99dc5d74a
sahithimuthyala <sahithi4@illinois.edu>
2020-03-06 17:00:28 -0600

    add searchable dropdowns

--- client/pages/projects/index.jsx
+++ client/pages/projects/index.jsx
@@ -7,12 +7,11 @@ import SelectSearch from "react-select-search";
 
 import "../../public/style.scss";
 import countryData from "../../utils/countries";
+import UNGoalData from "../../utils/goals";
+import groupSizeData from "../../utils/groups";
+import levelData from "../../utils/levels";
 import {
   Button,
-  ButtonDropdown,
-  Dropdown,
-  DropdownToggle,
-  DropdownMenu,
   DropdownItem,
   Card,
   CardBody,
@@ -25,119 +24,90 @@ import {
 } from "reactstrap";
 
 export default function(props) {
-  const [dropdownOpen1, setOpen1] = useState(false);
-  const toggle1 = () => setOpen1(!dropdownOpen1);
-
-  const [dropdownOpen2, setOpen2] = useState(false);
-  const toggle2 = () => setOpen2(!dropdownOpen2);
-
   const [dropdownOpen3, setOpen3] = useState(false);
   const toggle3 = () => setOpen3(!dropdownOpen3);
 
   const [dropdownOpen4, setOpen4] = useState(false);
   const toggle4 = () => setOpen4(!dropdownOpen4);
 
+  const populateUNGoals = () => {
+    let goals = new Array(17);
+
+    for (let i = 0; i < 17; i++) {
+      goals.push({
+        label: UNGoalData[i]["label"]
+      });
+    }
+
+    return goals;
+  };
+
   const populateLocations = () => {
     let locations = new Array(244);
 
     for (let i = 0; i < 244; i++) {
       locations.push({
-        name: countryData[i]["name"]
+        label: countryData[i]["label"]
       });
     }
 
     return locations;
   };
 
-  const locations = populateLocations();
+  const populateGroupOptions = () => {
+    let groups = new Array(5);
+
+    for (let i = 0; i < 5; i++) {
+      groups.push({
+        label: groupSizeData[i]["label"]
+      });
+    }
+
+    return groups;
+  };
+
+  const populateLevels = () => {
+    let levels = new Array(3);
+
+    for (let i = 0; i < 3; i++) {
+      levels.push({
+        label: levelData[i]["label"]
+      });
+    }
+
+    return levels;
+  };
 
   return (
     <>
       <Head title="Project Explorer" />
       <Container>
-        <div className="search-bar">
-          <Input type="text" className="input" placeholder="Find a project" />
+        <div class="form-inline">
+          <div className="search-bar">
+            <Input type="text" className="input" placeholder="Find a project" />
+          </div>
         </div>
         <div className="dropdowns">
-          <div className="dropdown-goals">
-            <ButtonDropdown id="goals" isOpen={dropdownOpen1} toggle={toggle1}>
-              <DropdownToggle caret>UN Goals</DropdownToggle>
-              <DropdownMenu>
-                <DropdownItem id="goal-1">No Poverty</DropdownItem>
-                <DropdownItem id="goal-2">Zero Hunger</DropdownItem>
-                <DropdownItem id="goal-3">
-                  Good Health and Well-Being
-                </DropdownItem>
-                <DropdownItem id="goal-4">Quality Education</DropdownItem>
-                <DropdownItem id="goal-5">Gender Equality</DropdownItem>
-                <DropdownItem id="goal-6">
-                  Clean Water and Sanitation
-                </DropdownItem>
-                <DropdownItem id="goal-7">
-                  Affordable and Clean Energy
-                </DropdownItem>
-                <DropdownItem id="goal-8">
-                  Decent Work and Economic Growth
-                </DropdownItem>
-                <DropdownItem id="goal-9">
-                  Industry, Innovation and Infrastructure
-                </DropdownItem>
-                <DropdownItem id="goal-10">Reduced Inequalities</DropdownItem>
-                <DropdownItem id="goal-11">
-                  Sustainable Cities and Communities
-                </DropdownItem>
-                <DropdownItem id="goal-12">
-                  Responsible Consumption and Production
-                </DropdownItem>
-                <DropdownItem id="goal-13">Climate Action</DropdownItem>
-                <DropdownItem id="goal-14">Life Below Water</DropdownItem>
-                <DropdownItem id="goal-15">Life On Land</DropdownItem>
-                <DropdownItem id="goal-16">
-                  Peace, Justice and Strong Institutions
-                </DropdownItem>
-                <DropdownItem id="goal-17">
-                  Partnerships for the Goals
-                </DropdownItem>
-              </DropdownMenu>
-            </ButtonDropdown>
-          </div>
-          <div className="dropdown-locations">
-            <Select
-              className="country"
-              options={locations}
-              placeholder="Search country"
-            />
-          </div>
-          <div className="dropdown-grp-sizes">
-            <ButtonDropdown
-              id="grp-sizes"
-              isOpen={dropdownOpen3}
-              toggle={toggle3}
-            >
-              <DropdownToggle caret>Group Size</DropdownToggle>
-              <DropdownMenu>
-                <DropdownItem id="under-5">Under 5 People</DropdownItem>
-                <DropdownItem id="5+">5+ People</DropdownItem>
-                <DropdownItem id="10+">10+ People</DropdownItem>
-                <DropdownItem id="20+">20+ People</DropdownItem>
-                <DropdownItem id="50+">50+ People</DropdownItem>
-              </DropdownMenu>
-            </ButtonDropdown>
-          </div>
-          <div className="dropdown-difficulty">
-            <ButtonDropdown
-              id="difficulty"
-              isOpen={dropdownOpen4}
-              toggle={toggle4}
-            >
-              <DropdownToggle caret>Difficulty</DropdownToggle>
-              <DropdownMenu>
-                <DropdownItem id="beginner">Beginner</DropdownItem>
-                <DropdownItem id="moderate">Moderate</DropdownItem>
-                <DropdownItem id="difficult">Difficult</DropdownItem>
-              </DropdownMenu>
-            </ButtonDropdown>
-          </div>
+          <Select
+            className="un-goals-list"
+            options={populateUNGoals()}
+            placeholder="Search UN Goals"
+          />
+          <Select
+            className="country-list"
+            options={populateLocations()}
+            placeholder="Search country"
+          />
+          <Select
+            className="grp-sizes-list"
+            options={populateGroupOptions()}
+            placeholder="Select group size"
+          />
+          <Select
+            className="difficulty-list"
+            options={populateLevels()}
+            placeholder="Select difficulty"
+          />
         </div>
       </Container>
     </>
---
+++ client/utils/goals.js
@@ -0,0 +1,19 @@
+export default [
+  { label: "No Poverty", value: "1" },
+  { label: "Zero Hunger", value: "2" },
+  { label: "Good Health and Well-Being", value: "3" },
+  { label: "Quality Education", value: "4" },
+  { label: "Gender Equality", value: "5" },
+  { label: "Clean Water and Sanitation", value: "6" },
+  { label: "Affordable and Clean Energy", value: "7" },
+  { label: "Decent Work and Economic Growth", value: "8" },
+  { label: "Industry, Innovation and Infrastructure", value: "9" },
+  { label: "Reduced Inequalities", value: "10" },
+  { label: "Sustainable Cities and Communities", value: "11" },
+  { label: "Responsible Consumption and Production", value: "12" },
+  { label: "Climate Action", value: "13" },
+  { label: "Life Below Water", value: "14" },
+  { label: "Life On Land", value: "15" },
+  { label: "Peace, Justice and Strong Institutions", value: "16" },
+  { label: "Partnerships for the Goals", value: "17" }
+];
