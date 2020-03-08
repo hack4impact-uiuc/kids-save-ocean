dc6b76cf3e616ed74fb135b7bb8d66a6cc9c1c26
ashankbehara <abehara2@illinois.edu>
2020-03-07 20:52:10 -0600

    Update index.jsx

--- client/components/index.jsx
+++ client/components/index.jsx
@@ -2,4 +2,4 @@ import Gantt from "./Gantt";
 import Head from "./Head";
 import TipCard from "./TipCard";
 import Stage from "./Stage";
-export { Gantt, Head, TipCard, Stage};
+export { Gantt, Head, TipCard, Stage };
