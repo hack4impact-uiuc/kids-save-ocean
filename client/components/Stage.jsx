ff8efe3a3d4a9bba9d04029c7fe618d34de1584f
abehara2 <46117114+abehara2@users.noreply.github.com>
2020-03-07 22:53:09 -0600

    Project page (#18)
    * install json schema validator for model creation endpoint
    
    * Create project model
    
    * implement json schema
    
    * fix import
    
    * Add get post and delete endpoints for models
    
    * Create model.js
    
    * Add API wrapper template
    
    * Completed model endpoints.
    
    * Resolved comments
    
    * Revert "Resolved comments"
    
    This reverts commit 3ebc3df0380a604f931e8b485b935e427727e665.
    
    * Fixed wrapper
    
    * Added wrapper exports for new endpoints
    
    * Fixed wrapper exports for new endpoints
    
    * Completed wrapper and queries:
    
    * formatted
    
    * formatted
    
    * lint:
    
    * Test lint client
    
    * Test lint
    
    * Completed
    
    * Completed comments and const
    
    * For Arpan to check
    
    * First iteration
    
    * Try build
    
    * Test build
    
    * Completed edit
    
    * Rename and Add Stage
    
    * Update test_model.js
    
    * Update index.jsx
    
    * Import style sheets
    
    * Update index.jsx
    
    * Update index.jsx
    
    * Refactor
    
    * Delete imports
    
    * Major Changes Styling
    
    * Remove logs
    
    * Formatting
    
    * fix css
    
    Co-authored-by: Brandon Wang <sourbrandon@gmail.com>
    Co-authored-by: ashwin1104 <ashwin.saxena1@gmail.com>
    Co-authored-by: Yousef Ahmed <yousefahmed0066@gmail.com>
    Co-authored-by: Arpan Laha <arpanlaha99@gmail.com>

---
+++ client/components/Stage.jsx
@@ -0,0 +1,18 @@
+import { Input, FormGroup, Label } from "reactstrap";
+// import "../public/styles/stage.scss";
+import "../public/styles/style.scss";
+import "../public/styles/phase-stage.scss";
+export default function(props) {
+  return (
+    <>
+      <FormGroup className="title">
+        <Label for="exampleText">
+          <h5 classname="header2-text">
+            <strong>{props.props[0]}</strong>
+          </h5>
+        </Label>
+        <Input type="textarea" name="text" id="exampleText" />
+      </FormGroup>
+    </>
+  );
+}
