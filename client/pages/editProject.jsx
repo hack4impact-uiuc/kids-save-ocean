9ed0aca3ff227bc78ac4984ccb97a86209b9af36
ashankbehara <abehara2@illinois.edu>
2020-03-09 20:49:07 -0500

    Completed imports:

--- client/pages/editProject.jsx
+++ client/pages/editProject.jsx
@@ -11,42 +11,38 @@ import {
   Container
 } from "reactstrap";
 import { getModelsByID } from "../utils/apiWrapper";
-import "../public/styles/home.scss";
 import "../public/styles/editProject.scss";
-import "../public/styles/style.scss";
 import { Head, Stage } from "../components";
 import { useState } from "react";
 export default function() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [title, setTitle] = useState("Loading");
-  const [description, setDescription] = useState("Loading");
+  const id = "5e653b729a1cbfaba98adc5b"
   //Stage
   let ideationStages = [
     ["Stage 1", "Description 1"],
     ["Stage 2", "Description 2"],
     ["Stage 3", "Description 3"]
   ];
-  const addStage = () => {
-    ideationStages.push(["Added Stage Title", "Added Stage Description"]);
-  };
   //Dropdown
   const toggle = () => setDropdownOpen(prevState => !prevState);
-  //Set Title
-  // const getTitle = async () => {
-  //   const project = await getModelsByID("5e5f20e2f182430b94c8c66d");
-  //   return project.data[0].name;
-  // };
-  // getTitle().then(name => {
-  //   name = setTitle(name);
-  // });
-  // //Set Description
-  // const getDescription = async () => {
-  //   const project = await getModelsByID("5e5f20e2f182430b94c8c66d");
-  //   return project.data[0].description;
-  // };
-  // getDescription().then(description => {
-  //   description = setDescription(description);
-  // });
+  //Get Title
+  const [title, setTitle] = useState("Loading");
+  const getTitle = async () => {
+    const project = await getModelsByID(id);
+    return project.data[0].name;
+  };
+  getTitle().then(name => {
+    name = setTitle(name);
+  });
+  //Set Description
+  const [description, setDescription] = useState("Loading");
+  const getDescription = async () => {
+    const project = await getModelsByID(id);
+    return project.data[0].description;
+  };
+  getDescription().then(description => {
+    description = setDescription(description);
+  });
   return (
     <>
       <Head title={title} />
