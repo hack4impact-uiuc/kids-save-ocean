2e47c437ba8d797161ddab93fae793a50745b379
ashwin1104 <34987983+ashwin1104@users.noreply.github.com>
2020-03-23 17:26:22 -0500

    Homepage and Create Project Pop-up Form (#24)
    * Update before master pull
    
    * some changes
    
    * fix lint errors
    
    * resolve comments
    
    Co-authored-by: Arpan Laha <arpanlaha99@gmail.com>

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Head } from "../components";
+import { Head, ProjectForm } from "../components";
 import Link from "next/link";
 import ReactPlayer from "react-player";
 import {
@@ -36,75 +36,73 @@ const populateImages = () => {
   return images;
 };
 
-export default function App() {
-  const images = populateImages();
+const images = populateImages();
+
+export default function Home() {
   return (
-    <>
+    <Container>
       <Head />
-      <Container>
-        <div className="page-title">
-          <h1 className="header2-text" align="center">
-            <strong>FateMaker</strong>
+      <div className="page-title" align="center">
+        <h1 className="header2-text" align="center">
+          <strong>FateMaker</strong>
+        </h1>
+      </div>
+      <Row className="home-block-1" type="flex" justify="center" xs="2">
+        <Col className="home-block-col">
+          <ReactPlayer url="https://www.youtube.com/watch?v=o08ykAqLOxk" />
+        </Col>
+        <Col className="home-block-col">
+          <Link href="#about-us">
+            <a className="text-link" href="#about-us">
+              ABOUT US
+            </a>
+          </Link>
+
+          <h1 className="header-text">Change the World,</h1>
+          <h1 className="header-text" mode="single">
+            one step at a time.
           </h1>
-        </div>
-        <Row className="home-block-1" type="flex" justify="center" xs="2">
-          <Col className="home-block-col">
-            <ReactPlayer url="https://youtu.be/dQw4w9WgXcQ" />
-          </Col>
-          <Col className="home-block-col">
-            <Link href="#about us">
-              <a className="text-link" href="/">
-                ABOUT US
-              </a>
-            </Link>
-            <h1 className="header-text">Change the World,</h1>
-            <h1 className="header-text" mode="single">
-              one step at a time.
-            </h1>
-            <p className="body-text">
-              The barred ratio flips under the proved nail. An umbrella brushes
-              a loading ash. An anarchy purchases the compromise against the
-              simulated cheek. The village parades beneath the breakdown.
-            </p>
-            <Row type="flex">
-              <Col span={18}>
-                <div className="home-page-btns">
-                  <Link href={"/projects"}>
-                    <Button type="primary" className="primary-btn">
-                      <strong>Find Projects</strong>
-                    </Button>
-                  </Link>
-                  <Button type="primary" className="second-btn">
-                    <strong>Start a Project</strong>
-                  </Button>
-                </div>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-        <Row className="sdg-row">
-          {images.map(sdgImage => (
-            <Col key={sdgImage.id} className="sdg-col" sm="2">
-              <CardGroup>
-                <Card
-                  className="sdg-card"
-                  tag="a"
-                  onClick={() => console.log("clicked")}
-                >
-                  <CardImg
-                    top
-                    width="100%"
-                    height="100%"
-                    src={sdgImage.imageLink}
-                    alt="Card image cap"
-                  />
-                </Card>
-              </CardGroup>
+          <p className="body-text">
+            The barred ratio flips under the proved nail. An umbrella brushes a
+            loading ash. An anarchy purchases the compromise against the
+            simulated cheek. The village parades beneath the breakdown.
+          </p>
+          <Row>
+            <Col xs="3"></Col>
+            <Col xs="2.5">
+              <Button type="primary" className="button-design">
+                <strong>Explore</strong>
+              </Button>
             </Col>
-          ))}
-        </Row>
-        <img src="/homepage-images/filler-map.png" alt="Map" />
-      </Container>
-    </>
+            <Col>
+              <ProjectForm></ProjectForm>
+            </Col>
+            <Col xs="2"></Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row className="sdg-row">
+        {images.map(sdgImage => (
+          <Col key={sdgImage.id} className="sdg-col" sm="2">
+            <CardGroup>
+              <Card
+                className="sdg-card"
+                tag="a"
+                onClick={() => console.log("clicked")}
+              >
+                <CardImg
+                  top
+                  width="100%"
+                  height="100%"
+                  src={sdgImage.imageLink}
+                  alt="Card image cap"
+                />
+              </Card>
+            </CardGroup>
+          </Col>
+        ))}
+      </Row>
+      <img src="/homepage-images/filler-map.png" alt="Map" />
+    </Container>
   );
 }
