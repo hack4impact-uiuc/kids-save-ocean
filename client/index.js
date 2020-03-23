5b889a970c9bb799d59840072cd9cbd80bb8a24f
Yousef <yousefahmed0066@gmail.com>
2020-03-23 01:03:20 -0500

    Remove mockdata.js and formatting

--- client/index.js
+++ client/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import App from 'pages/index.jsx';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "pages/index.jsx";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
