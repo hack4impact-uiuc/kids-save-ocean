28d545ca194391dba87081b950268fa3cfc5592a
Brandon Wang <sourbrandon@gmail.com>
2020-03-27 23:01:49 -0700

    fix?

--- api/auth/api/getSecurityQuestionForUser.js
+++ api/auth/api/getSecurityQuestionForUser.js
@@ -3,7 +3,7 @@ const { SERVER_URL } = require("./../utils/globalServerUrl");
 const router = require("express").Router();
 
 router.post("/getSecurityQuestionForUser", async function(req, res) {
-  const results = await fetch(`${SERVER_URL}/securityQuestionForUser`, {
+  const results = await fetch(`${SERVER_URL}/getSecurityQuestionForUser`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
