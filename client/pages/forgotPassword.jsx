7d91e5df548df69b773914d4fa03d718088fc127
Alice Fang <alicesf2@illinois.edu>
2020-04-02 15:43:08 -0500

    changed forgot password page to functional, added alert to wrong security question answer

--- client/pages/forgotPassword.jsx
+++ client/pages/forgotPassword.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import {
   getSecurityQuestionForUser,
@@ -21,57 +21,55 @@ import Router from "next/router";
 import { Head } from "../components";
 import "../public/styles/auth.scss";
 
-const EMAIL_REGEX =
-  "([a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)@([a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+).([a-zA-Z]{2,3}).?([a-zA-Z]{0,3})";
-// const PASSWORD_REGEX = "^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})";
-const SUCCESS = 200;
-class ForgotPasswordPage extends Component {
-  state = {
-    email: "",
-    question: "",
-    errorMessage: "",
-    answer: "",
-    pin: "",
-    password: "",
-    password2: "",
-    loadingAPI: false,
-    submitNewPassword: false
-  };
+export default function ForgotPasswordPage() {
+  // constants
+  const EMAIL_REGEX =
+    "([a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)@([a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+).([a-zA-Z]{2,3}).?([a-zA-Z]{0,3})";
+  // const PASSWORD_REGEX = "^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})";
+  const SUCCESS = 200;
 
-  handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
+  // state
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [pin, setPin] = useState("");
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [loadingAPI, setLoadingAPI] = useState(false);
+  const [submitNewPassword, setSubmitNewPassword] = useState(false);
+  const [successfulSubmit, setSuccessfulSubmit] = useState(false);
 
-  handleGetSecurityQuestion = async e => {
+  const handleGetSecurityQuestion = async e => {
     e.preventDefault();
-    const { email } = this.state;
     const result = await getSecurityQuestionForUser(email);
     const resp = await result.json();
     if (resp.question) {
-      this.setState({ question: resp.question, errorMessage: "" });
+      setQuestion(resp.question);
+      setErrorMessage("");
     } else {
-      this.setState({ errorMessage: resp.message });
+      setErrorMessage(resp.message);
     }
   };
 
-  handleSubmitSecurityAnswer = async e => {
+  const handleSubmitSecurityAnswer = async e => {
     e.preventDefault();
-    this.setState({ loadingAPI: true });
-    const { email, answer } = this.state;
+    setLoadingAPI(true);
     const result = await submitSecurityQuestionAnswer(email, answer);
     const resp = await result.json();
     if (resp.status === SUCCESS) {
-      this.setState({ submitNewPassword: true, errorMessage: "" });
+      setSubmitNewPassword(true);
+      setErrorMessage("");
     } else {
-      this.setState({ errorMessage: resp.message });
+      setLoadingAPI(false);
+      setErrorMessage(resp.message);
     }
   };
 
-  handleSubmitNewPassword = async e => {
+  const handleSubmitNewPassword = async e => {
     e.preventDefault();
-    const { password, password2, pin, email, answer } = this.state;
     if (password !== password2) {
-      this.setState({ errorMessage: "Passwords don't match!" });
+      setErrorMessage("Passwords don't match!");
       return;
     }
     const response = await (
@@ -79,29 +77,125 @@ class ForgotPasswordPage extends Component {
     ).json();
     if (response.status === SUCCESS && response.token) {
       setCookie("token", response.token);
-      this.setState({ successfulSubmit: true });
+      setSuccessfulSubmit(true);
       Router.push("/");
     } else {
-      this.setState({ errorMessage: response.message });
+      setErrorMessage(response.message);
     }
   };
+  return (
+    <div>
+      <Head />
+      {submitNewPassword ? (
+        <div className="auth-card-wrapper">
+          <Card className="auth-card">
+            <CardTitle>
+              <h3 className="auth-card-title">Reset Password</h3>
+            </CardTitle>
 
-  render = () => {
-    const {
-      errorMessage,
-      submitNewPassword,
-      pin,
-      password,
-      password2,
-      question,
-      email,
-      loadingAPI
-    } = this.state;
-    return (
-      <div>
-        <Head />
-        {submitNewPassword ? (
-          <div className="auth-card-wrapper">
+            <CardBody>
+              {errorMessage && (
+                <Alert className="auth-alert" color="danger">
+                  {errorMessage}
+                </Alert>
+              )}
+              <Form>
+                <FormGroup>
+                  <Label>Pin</Label>
+                  <Input
+                    name="pin"
+                    value={pin}
+                    onChange={e => setPin(e.target.value)}
+                    required
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label>Password</Label>
+                  <Input
+                    type="password"
+                    name="password"
+                    minLength="8"
+                    maxLength="64"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    required
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label>Confirm Password</Label>
+                  <Input
+                    type="password"
+                    name="password2"
+                    minLength="8"
+                    maxLength="64"
+                    value={password2}
+                    onChange={e => setPassword2(e.target.value)}
+                    required
+                  />
+                </FormGroup>
+                <Button
+                  color="success"
+                  size="m"
+                  onClick={handleSubmitNewPassword}
+                  className="mid-btn"
+                >
+                  Reset Password
+                </Button>
+              </Form>
+            </CardBody>
+            <div className="back-to-login">
+              <Link href="/login">
+                <a>Back to login page</a>
+              </Link>
+            </div>
+          </Card>
+        </div>
+      ) : (
+        <div className="auth-card-wrapper">
+          {question === "" ? (
+            <Card className="auth-card">
+              <CardTitle>
+                <h3 className="auth-card-title">Reset Password</h3>
+              </CardTitle>
+
+              <CardBody>
+                {errorMessage && (
+                  <Alert className="auth-alert" color="danger">
+                    {errorMessage}
+                  </Alert>
+                )}
+
+                <Form>
+                  <FormGroup>
+                    <Label>Email</Label>
+                    <Input
+                      type="email"
+                      name="email"
+                      id="exampleEmail"
+                      maxLength="64"
+                      pattern={EMAIL_REGEX}
+                      value={email}
+                      onChange={e => setEmail(e.target.value)}
+                      required
+                    />
+                  </FormGroup>
+                  <Button
+                    color="success"
+                    size="m"
+                    onClick={handleGetSecurityQuestion}
+                    className="mid-btn"
+                  >
+                    Get Security Question
+                  </Button>
+                </Form>
+              </CardBody>
+              <div className="back-to-login">
+                <Link href="/login">
+                  <a>Back to login page</a>
+                </Link>
+              </div>
+            </Card>
+          ) : (
             <Card className="auth-card">
               <CardTitle>
                 <h3 className="auth-card-title">Reset Password</h3>
@@ -115,45 +209,23 @@ class ForgotPasswordPage extends Component {
                 )}
                 <Form>
                   <FormGroup>
-                    <Label>Pin</Label>
+                    <p> {question}</p>
+                    <Label>Answer</Label>
                     <Input
-                      name="pin"
-                      value={pin}
-                      onChange={this.handleChange}
-                      required
-                    />
-                  </FormGroup>
-                  <FormGroup>
-                    <Label>Password</Label>
-                    <Input
-                      type="password"
-                      name="password"
-                      minLength="8"
-                      maxLength="64"
-                      value={password}
-                      onChange={this.handleChange}
-                      required
-                    />
-                  </FormGroup>
-                  <FormGroup>
-                    <Label>Confirm Password</Label>
-                    <Input
-                      type="password"
-                      name="password2"
-                      minLength="8"
-                      maxLength="64"
-                      value={password2}
-                      onChange={this.handleChange}
+                      type="answer"
+                      name="answer"
+                      onChange={e => setAnswer(e.target.value)}
                       required
                     />
                   </FormGroup>
                   <Button
                     color="success"
                     size="m"
-                    onClick={this.handleSubmitNewPassword}
+                    onClick={handleSubmitSecurityAnswer}
+                    disabled={loadingAPI}
                     className="mid-btn"
                   >
-                    Reset Password
+                    Submit Answer
                   </Button>
                 </Form>
               </CardBody>
@@ -163,92 +235,9 @@ class ForgotPasswordPage extends Component {
                 </Link>
               </div>
             </Card>
-          </div>
-        ) : (
-          <div className="auth-card-wrapper">
-            {question === "" ? (
-              <Card className="auth-card">
-                <CardTitle>
-                  <h3 className="auth-card-title">Reset Password</h3>
-                </CardTitle>
-
-                <CardBody>
-                  {errorMessage && (
-                    <Alert className="auth-alert" color="danger">
-                      {errorMessage}
-                    </Alert>
-                  )}
-
-                  <Form>
-                    <FormGroup>
-                      <Label>Email</Label>
-                      <Input
-                        type="email"
-                        name="email"
-                        id="exampleEmail"
-                        maxLength="64"
-                        pattern={EMAIL_REGEX}
-                        value={email}
-                        onChange={this.handleChange}
-                        required
-                      />
-                    </FormGroup>
-                    <Button
-                      color="success"
-                      size="m"
-                      onClick={this.handleGetSecurityQuestion}
-                      className="mid-btn"
-                    >
-                      Get Security Question
-                    </Button>
-                  </Form>
-                </CardBody>
-                <div className="back-to-login">
-                  <Link href="/login">
-                    <a>Back to login page</a>
-                  </Link>
-                </div>
-              </Card>
-            ) : (
-              <Card className="auth-card">
-                <CardTitle>
-                  <h3 className="auth-card-title">Reset Password</h3>
-                </CardTitle>
-
-                <CardBody>
-                  <Form>
-                    <FormGroup>
-                      <p> {question}</p>
-                      <Label>Answer</Label>
-                      <Input
-                        type="answer"
-                        name="answer"
-                        onChange={this.handleChange}
-                        required
-                      />
-                    </FormGroup>
-                    <Button
-                      color="success"
-                      size="m"
-                      onClick={this.handleSubmitSecurityAnswer}
-                      disabled={loadingAPI}
-                      className="mid-btn"
-                    >
-                      Submit Answer
-                    </Button>
-                  </Form>
-                </CardBody>
-                <div className="back-to-login">
-                  <Link href="/login">
-                    <a>Back to login page</a>
-                  </Link>
-                </div>
-              </Card>
-            )}
-          </div>
-        )}
-      </div>
-    );
-  };
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
-export default ForgotPasswordPage;
