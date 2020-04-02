31d482ebc03aa3e873b76e1cfa28a6fff6ce0048
Alice Fang <alicesf2@illinois.edu>
2020-04-02 15:23:51 -0500

    converted login page to functional

--- client/pages/login.jsx
+++ client/pages/login.jsx
@@ -1,3 +1,5 @@
+import React, { useEffect, useState } from "react";
+import { GoogleLogin } from "react-google-login";
 import Link from "next/link";
 import Router from "next/router";
 import { login, google } from "../utils/apiWrapper";
@@ -16,28 +18,24 @@ import { setCookie } from "./../utils/cookie";
 import { Head } from "../components";
 import "../public/styles/auth.scss";
 
-const EMAIL_REGEX =
-  "([a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)@([a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+).([a-zA-Z]{2,3}).?([a-zA-Z]{0,3})";
-const SUCCESS = 200;
-import { GoogleLogin } from "react-google-login";
-import React, { Component } from "react";
-class Login extends Component {
-  state = {
-    email: "",
-    password: "",
-    errorMessage: "",
-    username: ""
-  };
+export default function Login(props) {
+  // constants
+  const EMAIL_REGEX =
+    "([a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)@([a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+).([a-zA-Z]{2,3}).?([a-zA-Z]{0,3})";
+  const SUCCESS = 200;
+  const { role } = props;
 
-  handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
+  // state
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [username, setUsername] = useState("");
 
-  handleGoogle = async e => {
+  const handleGoogle = async e => {
     const result = await google(e.tokenId);
     const resp = await result.json();
     if (resp.status !== SUCCESS) {
-      this.setState({ errorMessage: resp.message });
+      setErrorMessage(resp.message);
     } else {
       setCookie("token", e.tokenId);
       setCookie("google", true);
@@ -45,104 +43,97 @@ class Login extends Component {
     }
   };
 
-  handleSubmit = async e => {
+  const handleSubmit = async e => {
     e.preventDefault();
-    const { email, password } = this.state;
-
     const result = await login(email, password);
     const resp = await result.json();
 
     if (!resp.token) {
-      this.setState({ errorMessage: resp.message });
+      setErrorMessage(resp.message);
     } else {
       setCookie("token", resp.token);
       Router.push("/");
     }
   };
 
-  render = () => {
-    const { email, password, errorMessage } = this.state;
-    const { role } = this.props;
-    return (
-      <div>
-        <Head />
-        <div className="auth-card-wrapper">
-          <Card className="auth-card">
-            <CardTitle>
-              <h3 className="auth-card-title">Login</h3>
-            </CardTitle>
+  return (
+    <div>
+      <Head />
+      <div className="auth-card-wrapper">
+        <Card className="auth-card">
+          <CardTitle>
+            <h3 className="auth-card-title">Login</h3>
+          </CardTitle>
 
-            <CardBody>
-              {errorMessage && (
-                <Alert className="auth-alert" color="danger">
-                  {errorMessage}
-                </Alert>
-              )}
-              <Form>
-                <FormGroup>
-                  <Label for="exampleEmail">Email</Label>
-                  <Input
-                    type="email"
-                    name="email"
-                    id="exampleEmail"
-                    maxLength="64"
-                    pattern={EMAIL_REGEX}
-                    value={email}
-                    onChange={this.handleChange}
-                    required
-                  />
-                </FormGroup>
-                <FormGroup>
-                  <Label for="examplePassword">Password</Label>
-                  <Input
-                    type="password"
-                    name="password"
-                    id="examplePassword"
-                    minLength="8"
-                    maxLength="64"
-                    value={password}
-                    onChange={this.handleChange}
-                    required
-                  />
-                </FormGroup>
-                <Button
-                  className="left-btn"
-                  color="success"
-                  size="m"
-                  onClick={this.handleSubmit}
-                >
-                  Log In
-                </Button>
-                <Button
-                  className="right-btn"
-                  color="success"
-                  size="m"
-                  onClick={() => Router.push("/register")}
-                >
-                  Register
-                </Button>
-              </Form>
-              <div className="forgot-password">
-                <Link href="/forgotPassword">
-                  <a>Forgot Password?</a>
-                </Link>
-              </div>
-            </CardBody>
-          </Card>
-        </div>
-        <div className="google-btn-wrapper">
-          <GoogleLogin
-            className="btn sign-in-btn"
-            clientId="992779657352-2te3be0na925rtkt8kt8vc1f8tiph5oh.apps.googleusercontent.com"
-            responseType="id_token"
-            buttonText={role}
-            scope="https://www.googleapis.com/auth/userinfo.email"
-            onSuccess={this.handleGoogle}
-          />
-        </div>
-        <br />
+          <CardBody>
+            {errorMessage && (
+              <Alert className="auth-alert" color="danger">
+                {errorMessage}
+              </Alert>
+            )}
+            <Form>
+              <FormGroup>
+                <Label for="exampleEmail">Email</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  id="exampleEmail"
+                  maxLength="64"
+                  pattern={EMAIL_REGEX}
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                  required
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="examplePassword">Password</Label>
+                <Input
+                  type="password"
+                  name="password"
+                  id="examplePassword"
+                  minLength="8"
+                  maxLength="64"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                  required
+                />
+              </FormGroup>
+              <Button
+                className="left-btn"
+                color="success"
+                size="m"
+                onClick={handleSubmit}
+              >
+                Log In
+              </Button>
+              <Button
+                className="right-btn"
+                color="success"
+                size="m"
+                onClick={() => Router.push("/register")}
+              >
+                Register
+              </Button>
+            </Form>
+            <div className="forgot-password">
+              <Link href="/forgotPassword">
+                <a>Forgot Password?</a>
+              </Link>
+            </div>
+          </CardBody>
+        </Card>
       </div>
-    );
-  };
+      <div className="google-btn-wrapper">
+        <GoogleLogin
+          className="btn sign-in-btn"
+          clientId="992779657352-2te3be0na925rtkt8kt8vc1f8tiph5oh.apps.googleusercontent.com"
+          responseType="id_token"
+          buttonText={role}
+          scope="https://www.googleapis.com/auth/userinfo.email"
+          onSuccess={handleGoogle}
+        />
+      </div>
+      <br />
+    </div>
+  );
 }
-export default Login;
