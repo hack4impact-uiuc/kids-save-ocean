78b13c49237f2040b90ac2f3b705ef32c55c6426
Alice Fang <32147571+alicesf2@users.noreply.github.com>
2020-04-21 00:37:17 -0500

    Fix auth and implement following backend (#57)
    * added auth frontend
    
    * started implementing following functionality
    
    * modified both user schemas to handle authentication, got register to work properly
    
    * forgot to add these files
    
    * fixed some linting errors
    
    * fixed user routes and api wrapper calls to handle auth properly
    
    * wrote endpoint for following projects
    
    * implemented get user's following projects, follow a project, and unfollow a project
    
    * fix linting errors
    
    * addressing comments
    
    * addressing feedback

--- client/utils/apiWrapper.js
+++ client/utils/apiWrapper.js
@@ -104,9 +104,15 @@ export const deleteForm = Model_ID => {
     });
 };
 
-export const register = (emailInput, passwordInput, questionIdx, answer) => {
+export const register = (
+  emailInput,
+  passwordInput,
+  questionIdx,
+  answer,
+  role
+) => {
   try {
-    return fetch(`${BASE_URL}/auth/register/`, {
+    return fetch(`${BASE_URL}/auth/register`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -114,8 +120,7 @@ export const register = (emailInput, passwordInput, questionIdx, answer) => {
         password: passwordInput,
         questionIdx,
         securityQuestionAnswer: answer,
-        role: "guest",
-        answer
+        role
       })
     });
   } catch (err) {
@@ -379,9 +384,9 @@ export const getDescription = (model_id, phaseName, stageName) => {
   }));
 };
 
-export const getUser = userId => {
+export const getUser = () => {
   try {
-    return fetch(`${BASE_URL}/users/${userId}`, {
+    return fetch(`${BASE_URL}/users/userInfo`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -400,19 +405,17 @@ export const createUser = newUser => {
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({
-        newUser
-      })
+      body: JSON.stringify(newUser)
     });
   } catch (err) {
     return err;
   }
 };
 
-export const updateUser = (userId, updatedUser) => {
+export const updateUser = updatedUser => {
   try {
     return (
-      fetch(`${BASE_URL}/users/${userId}`),
+      fetch(`${BASE_URL}/users/userInfo`),
       {
         method: "PUT",
         headers: {
@@ -427,9 +430,9 @@ export const updateUser = (userId, updatedUser) => {
   }
 };
 
-export const deleteUser = userId => {
+export const deleteUser = () => {
   try {
-    return fetch(`${BASE_URL}/users/${userId}`, {
+    return fetch(`${BASE_URL}/users/userInfo`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
