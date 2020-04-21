c89699a5dc2a164a01c52a65318f99c87c876e79
Alice Fang <alicesf2@illinois.edu>
2020-04-21 00:31:33 -0500

    addressing feedback

--- auth-server/src/utils/jwtHelpers.js
+++ auth-server/src/utils/jwtHelpers.js
@@ -13,11 +13,14 @@ function signAuthJWT(email, role) {
 function verifyAuthJWT(token, userEmail) {
   try {
     let { email } = jwt.verify(token, process.env.AUTH_SECRET);
+    if (!email) {
+      return false;
+    }
     if (String(userEmail) === String(email)) {
       return true;
     }
   } catch (err) {
-    console.log("Token was updated");
+    console.log("Invalid token.");
   }
   return false;
 }
@@ -28,7 +31,7 @@ function shouldUpdateJWT(token, email, role) {
     if (String(userEmail) === String(email) && userRole === role) {
       return false;
     }
-    return true;
+    return false;
   } catch (err) {
     return err;
   }
