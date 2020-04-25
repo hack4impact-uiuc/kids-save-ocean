45a002fbe005034ae4f3dbccb8d2e9e00193b0f4
Yousef <yousefahmed0066@gmail.com>
2020-04-25 14:53:35 -0500

    Fix linting

--- client/utils/validator.js
+++ client/utils/validator.js
@@ -5,13 +5,12 @@ export const checkValidUser = async () => {
   if (!localStorage.getItem("token")) {
     Router.replace("/login");
     return false;
-  } else {
-    const resp = await checkToken();
-    if (!resp.data.success) {
-      localStorage.removeItem("token");
-      Router.replace("/login");
-      return false;
-    }
+  }
+  const resp = await checkToken();
+  if (!resp.data.success) {
+    localStorage.removeItem("token");
+    Router.replace("/login");
+    return false;
   }
   return true;
 };
