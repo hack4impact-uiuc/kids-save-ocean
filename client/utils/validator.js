2809abebba815477e68ae4a15a141a41c366dcc1
Yousef Ahmed <yousefahmed0066@gmail.com>
2020-04-25 16:05:04 -0500

    Resolve users with invalid tokens to login page on proper requests (#69)
    * Add validator and api route
    
    * Add guards
    
    * Formatting
    
    * Fix logged in returning empty success field
    
    * Account for token checking being async
    
    * Fix linting
    
    * Missed 2 endpts
    
    * Add next if defined to move to next callback

---
+++ client/utils/validator.js
@@ -0,0 +1,16 @@
+import Router from "next/router";
+import { checkToken } from "./apiWrapper";
+
+export const checkValidUser = async () => {
+  if (!localStorage.getItem("token")) {
+    Router.replace("/login");
+    return false;
+  }
+  const resp = await checkToken();
+  if (!resp.data.success) {
+    localStorage.removeItem("token");
+    Router.replace("/login");
+    return false;
+  }
+  return true;
+};
