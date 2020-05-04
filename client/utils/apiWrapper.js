d21c17692514cae998143b630d97065e1f49a4ae
Yousef Ahmed <yousefahmed0066@gmail.com>
2020-05-04 17:55:36 -0500

    Proper token error handling (#78)
    * CheckToken returns proper error code now
    
    * Formatting and linting

--- client/utils/apiWrapper.js
+++ client/utils/apiWrapper.js
@@ -402,7 +402,6 @@ export const saveDescription = async (
 ) => {
   const validUser = await checkValidUser();
   if (validUser) {
-    console.log("SHOULD NOT SEE THIS");
     const requestString = `${BASE_URL}/models/${model_id}/${phaseName}/${stageName}/description`;
     console.log(requestString);
     return axios
@@ -649,10 +648,5 @@ export const checkToken = () => {
         "x-access-token": localStorage.getItem("token")
       }
     })
-    .catch(error => {
-      ({
-        type: "GET_TOKEN_CHECK_FAIL",
-        error
-      });
-    });
+    .catch(error => error.response);
 };
