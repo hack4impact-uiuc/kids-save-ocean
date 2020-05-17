186633c75c276158867a92c78b4431bc9aed1e85
ashwin1104 <ashwin.saxena1@gmail.com>
2020-05-17 18:43:29 -0500

    Finish carousel and begin card flip mechanism

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -6,18 +6,18 @@ import {
   Carousel,
   CarouselItem,
   CarouselControl,
-  CarouselIndicators,
-  CarouselCaption,
   Container,
   Row,
   Col,
   Card,
+  CardDeck,
   CardTitle,
   CardText,
   CardBody,
   CardGroup,
   CardImg
 } from "reactstrap";
+import ReactCardFlip from "react-card-flip";
 
 import UNGoals from "../utils/goals";
 import "../public/styles/home.scss";
@@ -26,32 +26,33 @@ import Router from "next/router";
 
 export default function Home() {
   const [validUser, setIsValidUser] = useState(false);
-  const [features, setFeatures] = useState([]);
-
+  const [isFlipped, setFlipped] = useState([]);
+  const [goals, setGoals] = useState([]);
   const [activeIndex, setActiveIndex] = useState(0);
   const [animating, setAnimating] = useState(false);
-  const items = [
-    {
-      src: "/homepage-images/stock-ocean.jpg",
-      altText: "Sustainability Rules!",
-      caption: "Sustainability Rules!"
-    },
-    {
-      src: "/homepage-images/stock-ocean.jpg",
-      altText: "Sustainability Rules!",
-      caption: "Sustainability Rules!"
-    },
-    {
-      src: "/homepage-images/stock-ocean.jpg",
-      altText: "Sustainability Rules!",
-      caption: "Sustainability Rules!"
-    }
-  ];
+  const numSlides = 5;
+  const numCardsPerSlide = 3;
+  const isRevolving = true;
+  const slideIndices = [...Array(numSlides).keys()];
+  // const sdgDescriptions = ["Economic growth must be inclusive to provide sustainable jobs and promote equality."]
+
+  useEffect(() => {
+    let sdgs = [...UNGoals];
+    sdgs.push({
+      label: "Sustainable Development Goals",
+      value: "18",
+      color: "#ffffff",
+      imageLink: "/sdg-images/sdg.png"
+    });
+    setGoals(sdgs);
+  }, []);
+
   const next = () => {
     if (animating) {
       return;
     }
-    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    const nextIndex =
+      activeIndex === slideIndices.length - 1 ? 0 : activeIndex + 1;
     setActiveIndex(nextIndex);
   };
 
@@ -59,49 +60,51 @@ export default function Home() {
     if (animating) {
       return;
     }
-    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    const nextIndex =
+      activeIndex === 0 ? slideIndices.length - 1 : activeIndex - 1;
     setActiveIndex(nextIndex);
   };
 
-  const goToIndex = newIndex => {
-    if (animating) {
-      return;
-    }
-    setActiveIndex(newIndex);
+  const handleClick = (e, value) => {
+    e.preventDefault();
+    let flips = [...isFlipped];
+    flips[value - 1] = !flips[value - 1];
+    setFlipped(flips);
   };
-
-  const slides = items.map(item => {
-    return (
-      <CarouselItem
-        onExiting={() => setAnimating(true)}
-        onExited={() => setAnimating(false)}
-        key={item.src}
-      >
-        <Card className="featured-card">
+  const slides = slideIndices.map(slideIndex => (
+    <CarouselItem
+      onExiting={() => setAnimating(true)}
+      onExited={() => setAnimating(false)}
+      key={slideIndex}
+    >
+      <CardDeck className="hp-featured-card-deck">
+        <Card className="hp-featured-card">
           <CardImg
-            width="100%"
-            src="/homepage-images/stock-ocean.jpg"
+            src="/homepage-images/feature-img.png"
             alt="Card image cap"
+            className="hp-card-img"
           ></CardImg>
-          <CardBody className="featured-body">
-            <CardTitle className="featured-caption-title">
-              <strong>Sustainable Recycling Project</strong>
+          <CardBody className="hp-featured-body">
+            <CardTitle className="hp-featured-caption-title">
+              <strong>
+                Sustainablility Rules! {slideIndex * numCardsPerSlide + 1}
+              </strong>
             </CardTitle>
-            <CardText className="featured-description">
-              A project which helps people take over the world and save it at
-              the same time.
+            <CardText className="hp-featured-description">
+              A project to help your school become more sustainable and
+              responsible.
             </CardText>
-            <div className="featured-footer">
-              <div className="featured-interactions">
+            <div className="hp-featured-footer">
+              <div className="hp-featured-interactions">
                 123{" "}
                 <img
-                  className="featured-comment-icon"
+                  className="hp-featured-comment-icon"
                   src="/feed-images/comment-icon.svg"
                   alt="comment"
                 />
                 123
                 <img
-                  className="featured-like-icon"
+                  className="hp-featured-like-icon"
                   src="/feed-images/like-icon.svg"
                   alt="like"
                 />
@@ -109,20 +112,82 @@ export default function Home() {
             </div>
           </CardBody>
         </Card>
-        {/* <img src={item.src} className="carousel-hp-image" alt={item.altText} />
-        <CarouselCaption
-          captionText={item.caption}
-          captionHeader={item.caption}
-        /> */}
-      </CarouselItem>
-    );
-  });
+        <Card className="hp-featured-card">
+          <CardImg
+            src="/homepage-images/feature-img.png"
+            alt="Card image cap"
+            className="hp-card-img"
+          ></CardImg>
+          <CardBody className="hp-featured-body">
+            <CardTitle className="hp-featured-caption-title">
+              <strong>
+                Sustainablility Rules! {(slideIndex + 1) * numCardsPerSlide - 1}
+              </strong>
+            </CardTitle>
+            <CardText className="hp-featured-description">
+              A project to help your school become more sustainable and
+              responsible.
+            </CardText>
+            <div className="hp-featured-footer">
+              <div className="hp-featured-interactions">
+                123{" "}
+                <img
+                  className="hp-featured-comment-icon"
+                  src="/feed-images/comment-icon.svg"
+                  alt="comment"
+                />
+                123
+                <img
+                  className="hp-featured-like-icon"
+                  src="/feed-images/like-icon.svg"
+                  alt="like"
+                />
+              </div>
+            </div>
+          </CardBody>
+        </Card>
+        <Card className="hp-featured-card">
+          <CardImg
+            src="/homepage-images/feature-img.png"
+            alt="Card image cap"
+            className="hp-card-img"
+          ></CardImg>
+          <CardBody className="hp-featured-body">
+            <CardTitle className="hp-featured-caption-title">
+              <strong>
+                Sustainablility Rules! {(slideIndex + 1) * numCardsPerSlide}
+              </strong>
+            </CardTitle>
+            <CardText className="hp-featured-description">
+              A project to help your school become more sustainable and
+              responsible.
+            </CardText>
+            <div className="hp-featured-footer">
+              <div className="hp-featured-interactions">
+                123{" "}
+                <img
+                  className="hp-featured-comment-icon"
+                  src="/feed-images/comment-icon.svg"
+                  alt="comment"
+                />
+                123
+                <img
+                  className="hp-featured-like-icon"
+                  src="/feed-images/like-icon.svg"
+                  alt="like"
+                />
+              </div>
+            </div>
+          </CardBody>
+        </Card>
+      </CardDeck>
+    </CarouselItem>
+  ));
 
   useEffect(() => {
     const checkCurrUser = async () => {
       setIsValidUser(await checkValidUser(false));
     };
-    console.log("HI");
     if (validUser) {
       Router.replace("/feed");
     }
@@ -130,11 +195,11 @@ export default function Home() {
   }, [validUser]);
 
   return (
-    <div>
+    <>
       <Head />
-      <div className="header-box">
-        <div className="header-title">Save the world, one step at a time</div>
-        <div className="header-subtitle">
+      <div className="home-main-box">
+        <div className="home-title">Save the world, one step at a time</div>
+        <div className="home-subtitle">
           We help you find and launch sustainability projects in your community.
         </div>
         <div>
@@ -150,11 +215,11 @@ export default function Home() {
           <img
             className="figures-img"
             src="/homepage-images/main-figures.png"
-            alt="Notifications"
+            alt="home-figures"
           ></img>
         </div>
       </div>
-      <div className="second-div">
+      <Container className="home-intro-box">
         <Row className="art-row">
           <Col className="figma-art-col-left">
             <img
@@ -164,112 +229,85 @@ export default function Home() {
             ></img>
           </Col>
           <Col className="figma-art-col-right">
-            {/* <Row className="figma-text">
-              <Col> */}
-            <Row className="about-text">ABOUT THE PROJECTS</Row>
-            <Row className="hcd-title">Human Centered Design</Row>
-            <Row className="hcd-desc">
-              We help you launch your sustainability project by using human
-              centered design. Projects will follow a process of Inspiration,
-              Ideation, and Implementation.
-            </Row>
-            {/* </Col>
-            </Row> */}
+            <div>
+              <Row className="about-text">ABOUT THE PROJECTS</Row>
+              <Row className="hcd-title">Human Centered Design</Row>
+              <Row className="hcd-desc">
+                We help you launch your sustainability project by using human
+                centered design. Projects will follow a process of Inspiration,
+                Ideation, and Implementation.
+              </Row>
+            </div>
           </Col>
         </Row>
+      </Container>
+      <div className="home-featured-box">
+        <div className="home-featured-title">Featured Projects</div>
+        <Carousel
+          activeIndex={activeIndex}
+          next={next}
+          previous={previous}
+          className="featured-carousel"
+        >
+          {slides}
+          <CarouselControl
+            direction="prev"
+            directionText="Previous"
+            onClickHandler={previous}
+          />
+          <CarouselControl
+            direction="next"
+            directionText="Next"
+            onClickHandler={next}
+          />
+        </Carousel>
       </div>
-      <div className="third-div">
-        <div className="hp-featured-title">Featured Projects</div>
-        <div className="hp-featured-cards">
-          <Carousel activeIndex={activeIndex} next={next} previous={previous}>
-            <CarouselIndicators
-              items={items}
-              activeIndex={activeIndex}
-              onClickHandler={goToIndex}
-            />
-            {slides}
-            <CarouselControl
-              direction="prev"
-              directionText="Previous"
-              onClickHandler={previous}
-            />
-            <CarouselControl
-              direction="next"
-              directionText="Next"
-              onClickHandler={next}
-            />
-          </Carousel>
-        </div>
-      </div>
-      <Container>
-        <div className="hp-featured-title">
-          The Sustainable Development Goals
-        </div>
-        <div className="hp-sdg-desc">
+      <Container className="home-sdg-box">
+        <div className="home-sdg-title">The Sustainable Development Goals</div>
+        <div className="home-sdg-desc">
           The UN has set 17 goals which promote country development while also
           protecting the Earth. These goals were adopted in 2015 alongside a
           15-year plan to achieve these goals. The SDGs serve as the core
           driving force behind the projects on FateMaker.
         </div>
-        {/* <Row className="home-block-1" type="flex" justify="center" xs="2">
-          <Col className="home-block-col">
-            <ReactPlayer url="https://www.youtube.com/watch?v=o08ykAqLOxk" />
-          </Col>
-          <Col className="home-block-col">
-            <Link href="#about-us">
-              <a className="text-link" href="#about-us">
-                ABOUT US
-              </a>
-            </Link>
-            <h1 className="header-text">Change the World,</h1>
-            <h1 className="header-text" mode="single">
-              one step at a time.
-            </h1>
-            <p className="body-text">
-              The barred ratio flips under the proved nail. An umbrella brushes
-              a loading ash. An anarchy purchases the compromise against the
-              simulated cheek. The village parades beneath the breakdown.
-            </p>
-            <Row>
-              <Col xs="3"></Col>
-              <Col xs="2.5">
-                <Link href="/projects">
-                  <Button type="primary" className="button-design">
-                    <a href="/projects">
-                      <strong>Explore</strong>
-                    </a>
-                  </Button>
-                </Link>
-              </Col>
-              <Col>
-                <ProjectForm></ProjectForm>
-              </Col>
-              <Col xs="2"></Col>
-            </Row>
-          </Col>
-  </Row> */}
         <Row className="sdg-row">
-          {UNGoals.map(sdg => (
+          {goals.map(sdg => (
             <Col key={sdg.value} className="sdg-col" sm="2">
-              <CardGroup>
-                <Card
-                  className="sdg-card"
-                  tag="a"
-                  onClick={() => console.log("clicked")}
-                >
-                  <CardImg
-                    top
-                    width="100%"
-                    height="100%"
-                    src={sdg.imageLink}
-                    alt="Card image cap"
-                  />
-                </Card>
-              </CardGroup>
+              <ReactCardFlip
+                key={sdg.value}
+                isFlipped={isFlipped[sdg.value - 1]}
+                flipDirection="horizontal"
+                infinite={isRevolving}
+              >
+                <CardGroup>
+                  <Card
+                    className="sdg-card"
+                    tag="a"
+                    onClick={e => handleClick(e, sdg.value)}
+                  >
+                    <CardImg
+                      top
+                      width="100%"
+                      height="100%"
+                      src={sdg.imageLink}
+                      alt="Card image cap"
+                    />
+                  </Card>
+                </CardGroup>
+                <CardGroup>
+                  <Card
+                    className="sdg-card"
+                    tag="a"
+                    onClick={e => handleClick(e, sdg.value)}
+                  >
+                    SDG {sdg.value} Description
+                  </Card>
+                </CardGroup>
+              </ReactCardFlip>
             </Col>
           ))}
         </Row>
       </Container>
-    </div>
+    </>
   );
 }
