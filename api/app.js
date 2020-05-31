77ba8ca0c7625055eff7e05043d8c62d3f9f118b
Yousef Ahmed <yousefahmed0066@gmail.com>
2020-05-31 15:55:28 -0500

    Update error handler

--- api/app.js
+++ api/app.js
@@ -81,27 +81,19 @@ app.use(function(err, req, res, next) {
   let responseData;
 
   if (err.name === "JsonSchemaValidation") {
-    // Log the error however you please
-    console.log(err.message);
-    // logs "express-jsonschema: Invalid data found"
 
-    // Set a bad request http response status or whatever you want
+    console.log(err.message);
+
     res.status(400);
 
-    // Format the response body however you want
     responseData = {
       statusText: "Bad Request",
       jsonSchemaValidation: true,
-      validations: err.validations // All of your validation information
+      validations: err.validations
     };
 
-    // Take into account the content type if your app serves various content types
     if (req.xhr || req.get("Content-Type") === "application/json") {
       res.json(responseData);
-    } else {
-      // If this is an html request then you should probably have
-      // some type of Bad Request html template to respond with
-      res.render("badrequestTemplate", responseData);
     }
   } else {
     // pass error to next error middleware handler
