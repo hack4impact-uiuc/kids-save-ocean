6aaae9713a9523f870512df58223102d5f47902a
Yousef Ahmed <yousefahmed0066@gmail.com>
2020-06-13 14:54:05 -0500

    Add sdg descriptions and styling

--- client/utils/goals.js
+++ client/utils/goals.js
@@ -2,102 +2,119 @@ export default [
   {
     label: "No Poverty",
     value: "1",
+    description: "To end poverty in all its forms everywhere.",
     color: "#e6253b",
     imageLink: "/sdg-images/1.png"
   },
   {
     label: "Zero Hunger",
     value: "2",
+    description: "End hunger, achieve food security and improved nutrition and promote sustainable agriculture.",
     color: "#dda63a",
     imageLink: "/sdg-images/2.png"
   },
   {
     label: "Good Health and Well-Being",
     value: "3",
+    description: "Ensure healthy lives and promote well-being for all at all ages.",
     color: "#4d9e37",
     imageLink: "/sdg-images/3.png"
   },
   {
     label: "Quality Education",
     value: "4",
+    description: "Ensure inclusive and quality education for all and promote lifelong learning.",
     color: "#c41b2f",
     imageLink: "/sdg-images/4.png"
   },
   {
     label: "Gender Equality",
     value: "5",
+    description: "Achieve gender equality and empower all women and girls.",
     color: "#ff3b21",
     imageLink: "/sdg-images/5.png"
   },
   {
     label: "Clean Water and Sanitation",
     value: "6",
+    description: "Ensure access to water and sanitation for all.",
     color: "#25bde3",
     imageLink: "/sdg-images/6.png"
   },
   {
     label: "Affordable and Clean Energy",
     value: "7",
+    description: "Ensure access to affordable, reliable, sustainable and modern energy for all.",
     color: "#fcc207",
     imageLink: "/sdg-images/7.png"
   },
   {
     label: "Decent Work and Economic Growth",
     value: "8",
+    description: "Promote inclusive and sustainable economic growth, employment and decent work for all.",
     color: "#a31942",
     imageLink: "/sdg-images/8.png"
   },
   {
     label: "Industry, Innovation and Infrastructure",
     value: "9",
+    description: "Build resilient infrastructure, promote sustainable industrialization and foster innovation.",
     color: "#fc6a27",
     imageLink: "/sdg-images/9.png"
   },
   {
     label: "Reduced Inequalities",
     value: "10",
+    description: "Reduce inequality within and among countries.",
     color: "#de1166",
     imageLink: "/sdg-images/10.png"
   },
   {
     label: "Sustainable Cities and Communities",
     value: "11",
+    description: "Making cities inclusive, safe, resilient, and sustainable.",
     color: "#fc9e25",
     imageLink: "/sdg-images/11.png"
   },
   {
     label: "Responsible Consumption and Production",
     value: "12",
+    description: "Ensure sustainable consumption and production patterns.",
     color: "#c08b2e",
     imageLink: "/sdg-images/12.png"
   },
   {
     label: "Climate Action",
     value: "13",
+    description: "Take urgent action to combat climate change and its impacts.",
     color: "#407d45",
     imageLink: "/sdg-images/13.png"
   },
   {
     label: "Life Below Water",
     value: "14",
+    description: "Conserve and sustainably use the oceans, seas and marine resources.",
     color: "#0996d9",
     imageLink: "/sdg-images/14.png"
   },
   {
     label: "Life On Land",
     value: "15",
+    description: "Sustainably manage forests, combat desertification, halt and reverse land degradation, halt biodiversity loss.",
     color: "#57bf2b",
     imageLink: "/sdg-images/15.png"
   },
   {
     label: "Peace, Justice and Strong Institutions",
     value: "16",
+    description: "Promote just, peaceful and inclusive societies.",
     color: "#00699e",
     imageLink: "/sdg-images/16.png"
   },
   {
     label: "Partnerships for the Goals",
     value: "17",
+    description: "Revitalize the global partnership for sustainable development.",
     color: "#1b476b",
     imageLink: "/sdg-images/17.png"
   }
