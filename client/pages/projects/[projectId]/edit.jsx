c82a1785475fcd0203aae79414cf4cf1186c1b23
Yousef Ahmed <yousefahmed0066@gmail.com>
2020-06-13 16:29:26 -0500

    Use projectId and push to /projects after delete

--- client/pages/projects/[projectId]/edit.jsx
+++ client/pages/projects/[projectId]/edit.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useCallback } from "react";
-import { useRouter } from "next/router";
+import Router, { useRouter } from "next/router";
 import Link from "next/link";
 import Select from "react-select";
 import classnames from "classnames";
@@ -54,8 +54,9 @@ export default WrappedMessage(function EditProjectPage(props) {
     setGrpSize(grpSize);
   };
 
-  const deleteProject = id => {
-    deleteForm(id);
+  const deleteProject = () => {
+    deleteForm(projectId);
+    Router.push("/projects");
   };
 
   const saveTopChanges = (name, description, groupSize) => {
